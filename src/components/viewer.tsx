43f5a0b254b5dbb827a845a86e48de93085824f1
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-03-14 17:00:38 +0100

    made mirador smaller to make room for text and annotations

--- src/components/viewer.tsx
+++ src/components/viewer.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import { MiradorInit } from "./MiradorInit";
+import './viewer.css';
 
 export function Viewer() {
     return (
-        <div>
+        <div id="mirador">
             <MiradorInit
                 config={{
                     id: 'mirador',
                     window: {
-                        sideBarPanel: 'info',
-                        sideBarOpen: true
+                        allowFullscreen: false,
                     },
                     windows: [
                         {
                             loadedManifest: "https://e-codices.unifr.ch/metadata/iiif/csg-0278/manifest.json",
                         },
                     ],
+                    thumbnailNavigation: {
+                        defaultPosition: 'far-bottom',
+                    },
+                    workspaceControlPanel: {
+                        enabled: false,
+                    }
                 }}
                 plugins={[]}
             />
