23f4bb0090be1009df6e95e68fec88948ea24329
Gijsjan <agijsbro@gmail.com>
2022-03-28 12:24:01 +0200

    Clean up comments

--- src/components/annotation.tsx
+++ src/components/annotation.tsx
@@ -1,41 +1,24 @@
 import React, { useContext } from 'react';
 import ann from '../data/ann1.json';
-//import { MiradorInit } from './MiradorInit';
 import { appContext } from '../state/context';
 import mirador from 'mirador';
 
-/*
-function bodyValue(annotation: any): string {
-    if (Array.isArray(annotation.body)) {
-        const body = annotation.body.find((b: { value: string; }) => b.value);
-        if (body) {
-            return body.value;
-        } else {
-            throw new Error('No body id found in ' + JSON.stringify(annotation));
-        }
-    } else {
-        return annotation.body.value;
-    }
-}
-*/
-
 export function Annotation(): any {
-    const { state } = useContext(appContext);
+    const { state } = useContext(appContext)
 
     const nextCanvas = () => {
-        const action = mirador.actions.setNextCanvas('republic');
-        state.store.dispatch(action);
+        const action = mirador.actions.setNextCanvas('republic')
+        state.store.dispatch(action)
     }
 
     const previousCanvas = () => {
-        const action = mirador.actions.setPreviousCanvas('republic');
-        state.store.dispatch(action);
+        const action = mirador.actions.setPreviousCanvas('republic')
+        state.store.dispatch(action)
     }
 
     const getCurrentCanvasId = () => {
-        const currentCanvasId = mirador.selectors.getCurrentCanvas();
-        //console.log(currentCanvasId);
-        return currentCanvasId;
+        const currentCanvasId = mirador.selectors.getCurrentCanvas()
+        return currentCanvasId
     }
 
     return (
@@ -53,6 +36,19 @@ export function Annotation(): any {
             </ol>
         </>
     )
-
-    //JSON.stringify(ann.items[0], null, 2);
 }
+
+/*
+function bodyValue(annotation: any): string {
+    if (Array.isArray(annotation.body)) {
+        const body = annotation.body.find((b: { value: string; }) => b.value);
+        if (body) {
+            return body.value;
+        } else {
+            throw new Error('No body id found in ' + JSON.stringify(annotation));
+        }
+    } else {
+        return annotation.body.value;
+    }
+}
+*/
