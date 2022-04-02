a4b48f2c9eb658945e0663178e4a424e6e305651
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-04-02 15:10:05 +0200

    Deleted placeholder annotation data

--- src/components/annotation.tsx
+++ src/components/annotation.tsx
@@ -1,13 +1,11 @@
 import React, { useContext } from 'react';
-import ann1 from '../data/attlist1ann1.json';
-import ann2 from '../data/res17ann1.json';
 import { appContext } from '../state/context';
 import mirador from 'mirador';
 import Elucidate from '../backend/Elucidate'
 
 export function Annotation(): any {
     const { state } = useContext(appContext)
-    
+
     const nextCanvas = () => {
         state.store.dispatch(mirador.actions.setNextCanvas('republic'))
         const currentState = state.store.getState()
@@ -33,7 +31,6 @@ export function Annotation(): any {
 
     const getCurrentCanvasId = () => {
         console.log(state.currentState)
-        console.log(ann2)
     }
 
     return (
@@ -41,14 +38,14 @@ export function Annotation(): any {
             <button onClick={nextCanvas}>Next canvas</button>
             <button onClick={previousCanvas}>Previous canvas</button>
             <button onClick={getCurrentCanvasId}>Get current canvas id</button>
-            <ol>
+            {/*<ol>
                 {
                     ann1.items.map((item, i) =>
                         <li key={i}>
                             <code>{JSON.stringify(item, null, '\t')}</code>
                         </li>
                     )}
-            </ol>
+                </ol>*/}
         </>
     )
 }
