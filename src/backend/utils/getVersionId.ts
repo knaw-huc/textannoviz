8056c85acd6e0d732458001c25a4eff95c2d1d89
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-08-01 13:41:14 +0200

    Removed obsolute files

--- src/backend/utils/getVersionId.ts
+++
@@ -1,7 +0,0 @@
-export default function getVersionId(id: string): string {
-    const result = id.match(/.*\/w3c\/([0-9a-f-]{36})\/([0-9a-f-]{36})/)?.[1] as string
-    if (!result) {
-        throw Error(`No version ID found in ${id}`)
-    }
-    return result
-}
