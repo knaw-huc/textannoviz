8ec1adec5d9411e090e22c819953e029b7323813
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-08-01 13:45:25 +0200

    Removed more obsolute files

--- src/backend/TextRepo.tsx
+++
@@ -1,9 +0,0 @@
-import { HOSTS } from "../Config"
-
-export default class TextRepo {
-    public static async getByVersionIdAndRange(id: string, start: number, end: number): Promise<string[]> {
-        const res = await fetch(`${HOSTS.TEXTREPO}/view/versions/${id}/segments/index/${start}/${end}`,)
-        
-        return await res.json()
-    }
-}
