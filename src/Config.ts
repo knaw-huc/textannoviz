9fe5b935b3e6407bd6e6c19fe1ea7a0fb93d0ea6
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-08-15 14:39:13 +0200

    Moved Broccoli url to config and removed local Elucidate and TextRepo urls in config

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,4 @@
 export const HOSTS = {
     RAA: "https://switch.sd.di.huc.knaw.nl/raa/persoon",
-    ELUCIDATE: "http://localhost:8000/elucidate",
-    TEXTREPO: "http://localhost:8000/textrepo"
+    BROCCOLI: "https://broccoli.tt.di.huc.knaw.nl/republic"
 }
--- src/backend/utils/fetchBroccoli.ts
+++ src/backend/utils/fetchBroccoli.ts
@@ -1,6 +1,7 @@
 import { fetchJson } from "./fetchJson"
+import { HOSTS } from "../../Config"
 
 export const fetchBroccoli = (opening = 285, volume = 1728) => {
-    const response = fetchJson(`https://broccoli.tt.di.huc.knaw.nl/republic/v1?opening=${opening}&volume=${volume}`)
+    const response = fetchJson(`${HOSTS.BROCCOLI}/v1?opening=${opening}&volume=${volume}`)
     return response
 }
