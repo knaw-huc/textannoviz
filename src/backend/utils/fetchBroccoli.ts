b5109b573993c8a280d2fd30af2da1a82a4e0c72
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-09-05 14:48:02 +0200

    Added fetch resolution function based on https://github.com/knaw-huc/broccoli/commit/67cfa11f7299dce9f45cf585231e1cf7b035f2a9

--- src/backend/utils/fetchBroccoli.ts
+++ src/backend/utils/fetchBroccoli.ts
@@ -5,3 +5,8 @@ export const fetchBroccoliOpening = (volume = "1728", opening = "285") => {
     const response = fetchJson(`${HOSTS.BROCCOLI}/v1?volume=${volume}&opening=${opening}`)
     return response
 }
+
+export const fetchBroccoliResolution = (volume = "1728", resolution = "urn:republic:session-1728-09-24-ordinaris-num-1-resolution-1") => {
+    const response = fetchJson(`${HOSTS.BROCCOLI}/v2/${volume}/resolution/${resolution}`)
+    return response
+}
