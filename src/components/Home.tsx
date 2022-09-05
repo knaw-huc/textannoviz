6fbff1cb85759544f122f437d534458e4826789b
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-09-05 14:47:26 +0200

    Changed default link in Homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <>
-            <Link to="detail">TextAnnoViz - Republic</Link>
+            <Link to="detail/1728/285">TextAnnoViz - Republic</Link>
         </>
     )
 }
