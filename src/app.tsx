78da5c1527d77d09905aa4d835d48d2e76471162
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-09-21 15:25:06 +0200

    WIP new routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,44 @@
 import React from "react"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from "react-router-dom"
 import { Detail } from "./Detail"
 import Home from "./components/Home"
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path: "detail/volumes/:volume/openings/:context",
-        element: <Detail />,
-        errorElement: <h2>Opening not found</h2>
-    },
-    {
-        path: "detail/resolutions/:resolutionId",
-        element: <Detail />,
-        errorElement: <h2>Resolution not found</h2>
-    }
-])
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <Home />
+//     },
+//     {
+//         path: "detail/volumes/:volume/openings/:context*",
+//         element: <Detail />,
+//         errorElement: <h2>Opening not found</h2>
+//     },
+//     {
+//         path: "detail/resolutions/:resolutionId*",
+//         element: <Detail />,
+//         errorElement: <h2>Resolution not found</h2>
+//     }
+// ])
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Routes>
+            <Route
+                key="Home"
+                path="/"
+                element={<Home />}
+            />
+            {["detail/volumes/:volume/openings/:context*", "detail/resolutions/:resolutionId*"].map(path => (
+                <Route
+                    key="Detail"
+                    path={path}
+                    element={<Detail />}
+                />
+            ))}
+        </Routes>
+
+    )
+)
 
 export default function App() {
     return <RouterProvider router={router} />
