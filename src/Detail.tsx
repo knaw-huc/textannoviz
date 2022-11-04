6858080a8b8ae970eb078c1ea1bd0953b74a335a
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-11-04 10:00:30 +0100

    MiradorContext changes

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -9,7 +9,7 @@ import { useMiradorContext } from "./components/Mirador/MiradorContext"
 import { useTextContext } from "./components/Text/TextContext"
 import { fetchBroccoliOpening, fetchBroccoliResolution } from "./backend/utils/fetchBroccoli"
 import { useParams } from "react-router-dom"
-import { BroccoliV2, OpeningRequest } from "./model/Broccoli"
+import { BroccoliV2 } from "./model/Broccoli"
 import { miradorConfig } from "./components/Mirador/MiradorConfig"
 import { visualizeAnnosMirador } from "./backend/utils/visualizeAnnosMirador"
 
@@ -24,7 +24,7 @@ const Row = styled.div`
     flex-direction: row;
 `
 
-function setMiradorConfig(broccoli: BroccoliV2) {
+const setMiradorConfig = (broccoli: BroccoliV2) => {
     miradorConfig.windows[0].loadedManifest = broccoli.iiif.manifest
     miradorConfig.windows[0].canvasId = broccoli.iiif.canvasIds[0]
 }
@@ -38,101 +38,59 @@ export const Detail = () => {
     const setTextState = useTextContext().setState
     const { volumeNum, openingNum, resolutionId } = useParams<{ volumeNum: string, openingNum: string, resolutionId: string }>()
 
+    const setState = React.useCallback((broccoli: BroccoliV2) => {
+        console.log(broccoli)
+        setMiradorConfig(broccoli)
+        const viewer = mirador.viewer(miradorConfig)
+        setMiradorState({
+            ...miradorState,
+            store: viewer.store
+        })
+        const iiifAnns = visualizeAnnosMirador(broccoli, viewer.store, broccoli.iiif.canvasIds[0])
+
+        setMiradorState({
+            ...miradorState,
+            mirAnn: iiifAnns
+        })
+
+        setMiradorState({
+            ...miradorState,
+            canvas: {
+                canvasIds: broccoli.iiif.canvasIds,
+                currentIndex: 0
+            }
+        })
+
+        setAnnotationsState({
+            ...annotationsState,
+            annotations: broccoli.anno
+        })
+
+        setTextState({
+            ...textState,
+            text: broccoli.text
+        })
+    }, [])
+
     React.useEffect(() => {
         if (volumeNum && openingNum) {
             fetchBroccoliOpening(volumeNum, openingNum)
-                .then(function (broccoli: BroccoliV2) {
-                    console.log(broccoli)
-                    setMiradorConfig(broccoli)
-                    const viewer = mirador.viewer(miradorConfig)
-                    setMiradorState({
-                        ...miradorState,
-                        store: viewer.store
-                    })
-                    const iiifAnns = visualizeAnnosMirador(broccoli, viewer.store, broccoli.iiif.canvasIds[0])
-
-                    setMiradorState({
-                        ...miradorState,
-                        mirAnn: iiifAnns
-                    })
-
-                    setMiradorState({
-                        ...miradorState,
-                        canvas: {
-                            canvasIds: broccoli.iiif.canvasIds,
-                            currentIndex: 0
-                        }
-                    })
-
-                    setMiradorState({
-                        ...miradorState,
-                        currentContext: {
-                            volume: (broccoli.request as OpeningRequest).volumeId,
-                            opening: (broccoli.request as OpeningRequest).openingNr
-                        }
-                    })
-
-                    setAnnotationsState({
-                        ...annotationsState,
-                        annotations: broccoli.anno
-                    })
-
-                    setTextState({
-                        ...textState,
-                        text: broccoli.text
-                    })
+                .then(broccoli => {
+                    setState(broccoli)
                 })
                 .catch(console.error)
         }
-    }, [volumeNum, openingNum])
+    }, [volumeNum, openingNum, setState])
 
     React.useEffect(() => {
         if (resolutionId) {
             fetchBroccoliResolution(resolutionId)
-                .then(function (broccoli: BroccoliV2) {
-                    console.log(broccoli)
-                    setMiradorConfig(broccoli)
-                    const viewer = mirador.viewer(miradorConfig)
-
-                    setMiradorState({
-                        ...miradorState,
-                        store: viewer.store
-                    })
-                    const iiifAnns = visualizeAnnosMirador(broccoli, viewer.store, broccoli.iiif.canvasIds[0])
-
-                    setMiradorState({
-                        ...miradorState,
-                        mirAnn: iiifAnns
-                    })
-
-                    setMiradorState({
-                        ...miradorState,
-                        canvas: {
-                            canvasIds: broccoli.iiif.canvasIds,
-                            currentIndex: 0
-                        }
-                    })
-
-                    setMiradorState({
-                        ...miradorState,
-                        currentContext: {
-                            volume: (broccoli.request as OpeningRequest).volumeId,
-                            opening: (broccoli.request as OpeningRequest).openingNr
-                        }
-                    })
-
-                    setAnnotationsState({
-                        ...annotationsState,
-                        annotations: broccoli.anno
-                    })
-
-                    setTextState({
-                        ...textState,
-                        text: broccoli.text
-                    })
+                .then(broccoli => {
+                    setState(broccoli)
                 })
+                .catch(console.error)
         }
-    }, [resolutionId])
+    }, [resolutionId, setState])
 
     return (
         <AppContainer id="appcontainer">
--- src/components/Mirador/MiradorContext.ts
+++ src/components/Mirador/MiradorContext.ts
@@ -4,9 +4,7 @@ import { ContextType } from "../../backend/utils/ContextType"
 import { dummyProvider } from "../../backend/utils/dummyProvider"
 import { baseReducer } from "../../backend/utils/baseReducer"
 
-export const useMiradorContext = () => React.useContext(MiradorContext)
-
-export type MiradorStateType = {
+interface MiradorStateType {
     store: any,
     mirAnn: iiifAnn | undefined,
     canvas: {
@@ -19,21 +17,87 @@ export type MiradorStateType = {
     }
 }
 
-export const defaultMiradorContext = {
-    state: {
-        store: undefined,
-        mirAnn: undefined,
-        canvas: {
-            canvasIds: [],
-            currentIndex: 0
-        },
-        currentContext: {
-            volume: "",
-            opening: 0
-        }
+const initialMiradorState: MiradorStateType = {
+    store: undefined,
+    mirAnn: undefined,
+    canvas: {
+        canvasIds: [],
+        currentIndex: 0
     },
-    setState: dummyProvider
-} as ContextType<MiradorStateType>
+    currentContext: {
+        volume: "",
+        opening: 0
+    }
+}
 
-export const MiradorContext = React.createContext(defaultMiradorContext)
-export const miradorReducer : (<T extends MiradorStateType>(s: T, a: T) => T) = baseReducer
+interface MiradorContext {
+    state: MiradorStateType,
+    dispatch: React.Dispatch<MiradorAction>
+}
+
+enum MiradorActions {
+    SET_STORE = "SET_STORE",
+    SET_MIRANN = "SET_MIRANN",
+    SET_CANVAS = "SET_CANVAS",
+    SET_CURRENTCONTEXT = "SET_CURRENTCONTEXT"
+}
+
+export type SetStore =
+    Pick<MiradorStateType, "store"> &
+    {
+        type: MiradorActions.SET_STORE
+    }
+
+export type SetMirAnn =
+    Pick<MiradorStateType, "mirAnn"> &
+    {
+        type: MiradorActions.SET_MIRANN
+    }
+
+export type SetCanvas =
+    Pick<MiradorStateType, "canvas"> &
+    {
+        type: MiradorActions.SET_CANVAS
+    }
+
+export type SetCurrentContext =
+    Pick<MiradorStateType, "currentContext"> &
+    {
+        type: MiradorActions.SET_CURRENTCONTEXT
+    }
+
+type MiradorAction = SetStore | SetMirAnn | SetCanvas | SetCurrentContext
+
+// export const useMiradorContext = () => React.useContext(MiradorContext)
+
+// export type MiradorStateType = {
+//     store: any,
+//     mirAnn: iiifAnn | undefined,
+//     canvas: {
+//         canvasIds: string[],
+//         currentIndex: number
+//     },
+//     currentContext: {
+//         volume: string,
+//         opening: string | number
+//     }
+// }
+
+// export const defaultMiradorContext = {
+//     state: {
+//         store: undefined,
+//         mirAnn: undefined,
+//         canvas: {
+//             canvasIds: [],
+//             currentIndex: 0
+//         },
+//         currentContext: {
+//             volume: "",
+//             opening: 0
+//         }
+//     },
+//     setState: dummyProvider
+// } as ContextType<MiradorStateType>
+
+// export const MiradorContext = React.createContext(defaultMiradorContext)
+// export const miradorReducer : (<T extends MiradorStateType>(s: T, a: T) => T) = baseReducer
