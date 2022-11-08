f32fe1c8ea50158637fe21ba3ad423bc30a1362c
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-11-08 15:49:49 +0100

    Removed console log from Home and bump to v.0.1.1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default function Home() {
-    console.log(process.env.DTAP)
     return (
         <>
             <Link to="detail/volumes/1728/openings/285">TextAnnoViz - Republic</Link>
