b070fe33edbbe53742d3b78cfc7723fe0371ce18
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-11-18 11:25:23 +0100

    Removed obsolete files related to Mirador reducer

--- src/state/mirador/set-mirann.ts
+++
@@ -1,9 +0,0 @@
-import { SetMirAnn } from "./MiradorActions"
-import { MiradorState } from "./MiradorState"
-
-export const setMirAnn = (miradorState: MiradorState, action: SetMirAnn) => {
-    return {
-        ...miradorState,
-        mirAnn: action.mirAnn
-    }
-}
--- src/state/mirador/set-store.ts
+++
@@ -1,9 +0,0 @@
-import { SetStore } from "./MiradorActions"
-import { MiradorState } from "./MiradorState"
-
-export const setStore = (miradorState: MiradorState, action: SetStore) => {
-    return {
-        ...miradorState,
-        store: action.store
-    }
-}
