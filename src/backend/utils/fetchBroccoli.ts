e158d9078b87f2e94ebfec075885eb6aafed8e90
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2022-11-30 15:49:03 +0100

    Added with annotation types to fetch. Moved logic away from broccoli back to frontend

--- src/backend/utils/fetchBroccoli.ts
+++ src/backend/utils/fetchBroccoli.ts
@@ -1,28 +1,45 @@
-import { HOSTS } from "../../Config"
+import { HOSTS } from "../../Config";
 
-export const fetchBroccoliOpening = async (volume = "1728", opening = "285") => {
-    if (parseInt(opening) < 1) {
-        alert("Opening number lower than 1 is not allowed!")
-        return
-    }
+export const fetchBroccoliOpening = async (
+  volume = "1728",
+  opening = "285"
+) => {
+  if (parseInt(opening) < 1) {
+    alert("Opening number lower than 1 is not allowed!");
+    return;
+  }
 
-    const response = await fetch(`${HOSTS.BROCCOLI}/v3/volumes/${volume}/openings/${opening}`)
-    if (!response.ok) {
-        const error = await response.json()
-        alert(`${error.message}`)
-        return
-    }
-    return response.json()
-}
+  const annotationTypesToInclude = [
+    "Session, Resolution, Reviewed, AttendanceList, Attendant",
+  ]; //Exclude: "Line", "Page", "RepublicParagraph", "TextRegion", "Scan"
 
-export const fetchBroccoliResolution = async (resolutionId = "urn:republic:session-1728-09-24-ordinaris-num-1-resolution-1") => {
-    const response = await fetch(`${HOSTS.BROCCOLI}/v3/bodies/${resolutionId}`)
-    if (!response.ok) return null
-    return response.json()
-}
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/v3/volumes/${volume}/openings/${opening}?includeTypes=${annotationTypesToInclude}`
+  );
+  if (!response.ok) {
+    const error = await response.json();
+    alert(`${error.message}`);
+    return null;
+  }
+  return response.json();
+};
 
-export const fetchBroccoliBodyId = async (volume = "1728", opening = "285", bodyId = "urn:republic:session-1728-06-19-ordinaris-num-1-para-2") => {
-    const response = await fetch(`${HOSTS.BROCCOLI}/v2?volume=${volume}&opening=${opening}&bodyId=${bodyId}`)
-    if (!response.ok) return null
-    return response.json()
-}
+export const fetchBroccoliResolution = async (
+  resolutionId = "urn:republic:session-1728-09-24-ordinaris-num-1-resolution-1"
+) => {
+  const response = await fetch(`${HOSTS.BROCCOLI}/v3/bodies/${resolutionId}`);
+  if (!response.ok) return null;
+  return response.json();
+};
+
+export const fetchBroccoliBodyId = async (
+  volume = "1728",
+  opening = "285",
+  bodyId = "urn:republic:session-1728-06-19-ordinaris-num-1-para-2"
+) => {
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/v2?volume=${volume}&opening=${opening}&bodyId=${bodyId}`
+  );
+  if (!response.ok) return null;
+  return response.json();
+};
