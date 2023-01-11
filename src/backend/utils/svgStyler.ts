a10394e7abcfe485f78fe67e29ac3521b0cf8ead
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-01-11 15:04:27 +0100

    Check if path has data in svgStyler

--- src/backend/utils/svgStyler.ts
+++ src/backend/utils/svgStyler.ts
@@ -1,14 +1,16 @@
 export const svgStyler = (svg: string, colour: string): string => {
-    const parser = new DOMParser()
-    const serializer = new XMLSerializer()
-    
-    const doc = parser.parseFromString(svg, "application/xml")
-    const path = doc.getElementsByTagName("path")
+  const parser = new DOMParser();
+  const serializer = new XMLSerializer();
 
-    path[0].setAttribute("stroke", colour)
-    path[0].setAttribute("fill", colour)
-    path[0].setAttribute("fill-opacity", "0.5")
-    path[0].setAttribute("stroke-width", "1")
+  const doc = parser.parseFromString(svg, "application/xml");
+  const path = doc.getElementsByTagName("path");
 
-    return serializer.serializeToString(doc)
-}
+  if (path.length > 0) {
+    path[0].setAttribute("stroke", colour);
+    path[0].setAttribute("fill", colour);
+    path[0].setAttribute("fill-opacity", "0.5");
+    path[0].setAttribute("stroke-width", "1");
+  }
+
+  return serializer.serializeToString(doc);
+};
