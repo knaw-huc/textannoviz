8d61c1dda84bfb7e016fd7812e7609aa485fb3ce
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-01-11 14:42:05 +0100

    Added ToastContainer to DOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./app";
 import "./index.css";
 
 document.addEventListener("DOMContentLoaded", function () {
   ReactDOM.render(
     <React.StrictMode>
+      <ToastContainer />
       <App />
     </React.StrictMode>,
     document.getElementById("container")
