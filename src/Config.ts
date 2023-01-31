f1d3c082acf3332c8ca852b3afed5e359cc26464
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-01-31 13:46:15 +0100

    Removed project name from Broccoli URL in config

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
 export const HOSTS = {
-    RAA: "https://switch.sd.di.huc.knaw.nl/raa/persoon",
-    BROCCOLI: "https://broccoli.tt.di.huc.knaw.nl/republic"
-}
+  RAA: "https://switch.sd.di.huc.knaw.nl/raa/persoon",
+  BROCCOLI: "https://broccoli.tt.di.huc.knaw.nl",
+};
