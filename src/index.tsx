4531a6ba35860c1dad38a50474d29359870f3d93
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-02-27 13:39:59 +0100

    Switch ReactDOM.render to new createRoot API

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./app";
 import "./index.css";
 
-document.addEventListener("DOMContentLoaded", function () {
-  ReactDOM.render(
-    <React.StrictMode>
-      <ToastContainer />
-      <App />
-    </React.StrictMode>,
-    document.getElementById("container")
-  );
-});
+const container = document.getElementById("container");
+
+// "createRoot(container!) is recommended by React docs: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html. See line: "const root = createRoot(container); // createRoot(container!) if you use TypeScript"".
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    <ToastContainer />
+    <App />
+  </React.StrictMode>
+);
