fae43e036f3092f5794356a23de188594c9e8d3b
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-03-20 11:42:52 +0100

    Fixed build scripts

--- docker-republic-all.sh
+++ docker-republic-all.sh
@@ -4,7 +4,7 @@ VERSION=$(npm pkg get version | tr -d \")
 DOCKER_DOMAIN=registry.diginfra.net/tt
 PROJECT=republic
 
-find .env -type f -exec sed -i '' -e "/^PROJECT=/s/=.*/=$PROJECT/" {} \;
+find .env -type f -exec sed -i '' -e "/^VITE_PROJECT=/s/=.*/=$PROJECT/" {} \;
 
 #Uncomment this line if running this script with the GNU version of sed. See: https://riptutorial.com/sed/topic/9436/bsd-macos-sed-vs--gnu-sed-vs--the-posix-sed-specification
 #find .env -type f -exec sed -i "/^PROJECT=/s/=.*/=$PROJECT/" {} \;
