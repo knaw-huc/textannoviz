20ca8889ffa047391bd5b1c1391f2f11797529ba
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-03-20 15:27:56 +0100

    Added list of all available Republic scans to search page as a mock

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { createIndices } from "../../utils/createIndices";
 
 export const Search = () => {
-  return <div>Search page</div>;
+  const indices = createIndices(51, 532);
+  console.log(indices);
+  return (
+    <div style={{ display: "inline" }}>
+      {indices.map((index, key) => (
+        <>
+          <Link key={key} to={`/detail/1728/${index}`}>
+            Scan {index}
+          </Link>
+          <br />
+        </>
+      ))}
+    </div>
+  );
 };
