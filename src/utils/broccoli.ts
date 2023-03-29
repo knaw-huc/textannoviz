4310406bebb2c006c7377fe8cdc0af8adcfd5658
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-03-29 10:51:24 +0200

    Renamed broccoli function and added search function

---
+++ src/utils/broccoli.ts
@@ -0,0 +1,100 @@
+import { toast } from "react-toastify";
+import { HOSTS } from "../Config";
+import { ProjectConfig } from "../model/ProjectConfig";
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export const fetchBroccoliBodyId = async (
+  bodyId: string,
+  config: ProjectConfig
+) => {
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/projects/${config.id}/${bodyId}`
+  );
+  if (!response.ok) {
+    const error = await response.json();
+    toast(`${error.message}`, { type: "error" });
+    return null;
+  }
+  return response.json();
+};
+
+export const fetchBroccoliBodyIdRelativeTo = async (
+  bodyId: string,
+  relativeTo: string,
+  includeResults: string[],
+  config: ProjectConfig
+) => {
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/projects/${config.id}/${bodyId}?includeResults=${includeResults}&relativeTo=${relativeTo}`
+  );
+  if (!response.ok) {
+    const error = await response.json();
+    toast(`${error.message}`, { type: "error" });
+    return null;
+  }
+  return response.json();
+};
+
+export const fetchBroccoliBodyIdOfScan = async (
+  tier0: string,
+  tier1: string,
+  config: ProjectConfig
+) => {
+  if (parseInt(tier1) < 1) {
+    toast("Opening number lower than 1 is not allowed!", { type: "error" });
+    return;
+  }
+
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/projects/${config.id}/${config.scanAnnotation}/${tier0}/${tier1}?includeResults=bodyId`
+  );
+  if (!response.ok) {
+    const error = await response.json();
+    toast(`${error.message}`, { type: "error" });
+    return null;
+  }
+  return response.json();
+};
+
+export const fetchBroccoliScanWithOverlap = async (
+  bodyId: string,
+  overlapTypes: string[],
+  includeResults: string[],
+  config: ProjectConfig
+) => {
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/projects/${config.id}/${bodyId}?overlapTypes=${overlapTypes}&includeResults=${includeResults}&relativeTo=${config.relativeTo}`
+  );
+  if (!response.ok) {
+    const error = await response.json();
+    toast(`${error.message}`, { type: "error" });
+    return null;
+  }
+  return response.json();
+};
+
+export const doSearch = async (searchQuery: any, config: ProjectConfig) => {
+  const response = await fetch(
+    `${HOSTS.BROCCOLI}/projects/${config.id}/search`,
+    {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(searchQuery),
+    }
+  );
+
+  console.log(response);
+
+  if (!response.ok) {
+    const error = await response.json();
+    toast(`${error.message}`, { type: "error" });
+    return null;
+  }
+
+  const data = await response.json();
+
+  return data;
+};
