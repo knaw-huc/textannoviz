50d242d619a6da7a1d620137bc8256b755cae66f
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-05-22 11:02:02 +0200

    Style changes

--- src/components/Search/SearchSortBy.tsx
+++ src/components/Search/SearchSortBy.tsx
@@ -6,7 +6,7 @@ export const SearchSortBy = (props: SearchSortByProps) => {
   return (
     <div className="sortBy">
       Sort by:
-      <select onChange={props.onChange}>
+      <select onChange={props.onChange} style={{ marginLeft: "5px" }}>
         <option value="_score">Relevance</option>
         <option value="dateAsc">Date (asc)</option>
         <option value="dateDesc">Date (desc)</option>
