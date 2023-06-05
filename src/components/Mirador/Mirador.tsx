a14d2a7086325c70a6fe44e4a44c7362b67e5909
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-06-05 16:20:29 +0200

    Style changes

--- src/components/Mirador/Mirador.tsx
+++ src/components/Mirador/Mirador.tsx
@@ -2,8 +2,12 @@ import styled from "styled-components";
 
 const MiradorStyled = styled.div`
   position: relative;
-  min-width: 500px;
+  width: 30%;
   height: 800px;
+  flex-grow: 1;
+  align-self: stretch;
+  max-height: 100vh;
+  top: 0px;
 `;
 export function Mirador() {
   return <MiradorStyled id="mirador" />;
