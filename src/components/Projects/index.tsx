ec36de3e64857a3c0ff99ee27471287a3dc49698
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-06-19 10:42:34 +0200

    Removed unused file

--- src/components/Projects/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from "../Search/Search";
-import { republicConfig } from "./republic/config";
-
-export const Projects = () => {
-  const project = "republic";
-  const config = republicConfig;
-
-  return <Search project={project} projectConfig={config} />;
-};
