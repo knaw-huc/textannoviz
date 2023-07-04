dca01a5ea966f1e8df03bce6ed6e81ec61243243
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-07-04 12:14:23 +0200

    Removed unused text state

--- src/stores/text.ts
+++ src/stores/text.ts
@@ -1,29 +1,10 @@
 import { create, StateCreator } from "zustand";
-import { Broccoli, BroccoliTextGeneric } from "../model/Broccoli";
-
-export type TextSlice = {
-  text: BroccoliTextGeneric;
-  setText: (newText: TextSlice["text"]) => void;
-};
+import { Broccoli } from "../model/Broccoli";
 
 export type TextPanelsSlice = {
   views: Broccoli["views"] | undefined;
   setViews: (newViews: TextPanelsSlice["views"]) => void;
 };
-
-const createTextSlice: StateCreator<TextSlice, [], [], TextSlice> = (set) => ({
-  text: {
-    lines: [],
-    locations: {
-      relativeTo: {
-        bodyType: "",
-        bodyId: "",
-      },
-      annotations: [],
-    },
-  },
-  setText: (newText) => set(() => ({ text: newText })),
-});
 
 const createTextPanelsSlice: StateCreator<
   TextPanelsSlice,
@@ -35,7 +16,6 @@ const createTextPanelsSlice: StateCreator<
   setViews: (newViews) => set(() => ({ views: newViews })),
 });
 
-export const useTextStore = create<TextSlice & TextPanelsSlice>()((...a) => ({
-  ...createTextSlice(...a),
+export const useTextStore = create<TextPanelsSlice>()((...a) => ({
   ...createTextPanelsSlice(...a),
 }));
