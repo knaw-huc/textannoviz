ad79e476d571995fd52faffa20f8ca45da1ce0e5
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-07-24 16:33:02 +0200

    First step towards multi config tailwindcss

---
+++ tailwind.config.republic.js
@@ -0,0 +1,60 @@
+import typography from "@tailwindcss/typography";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        public: ["Public Sans", "sans-serif"],
+      },
+      colors: {
+        rpBrand1: {
+          50: "hsl(48, 45%, 97%)",
+          100: "hsl(48, 45%, 90%)",
+          200: "hsl(48, 45%, 80%)",
+          300: "hsl(48, 45%, 70%)",
+          400: "hsl(48, 45%, 60%)",
+          500: "hsl(48, 45%, 52%)",
+          600: "hsl(48, 45%, 40%)",
+          700: "hsl(48, 45%, 30%)",
+          800: "hsl(48, 45%, 20%)",
+          900: "hsl(48, 45%, 10%)",
+        },
+        rpBrand1grey: {
+          50: "hsl(48, 15%, 97%)",
+          100: "hsl(48, 15%, 90%)",
+          200: "hsl(48, 15%, 80%)",
+          300: "hsl(48, 15%, 70%)",
+          400: "hsl(48, 15%, 60%)",
+          500: "hsl(48, 15%, 52%)",
+          600: "hsl(48, 15%, 40%)",
+          700: "hsl(48, 15%, 30%)",
+          800: "hsl(48, 15%, 20%)",
+          900: "hsl(48, 15%, 10%)",
+        },
+        rpBrand2: {
+          50: "hsl(195, 30%, 97%)",
+          100: "hsl(195, 30%, 90%)",
+          200: "hsl(195, 30%, 80%)",
+          300: "hsl(195, 30%, 70%)",
+          400: "hsl(195, 30%, 60%)",
+          500: "hsl(195, 30%, 52%)",
+          600: "hsl(195, 30%, 40%)",
+          700: "hsl(195, 30%, 30%)",
+          800: "hsl(195, 30%, 20%)",
+          900: "hsl(195, 30%, 10%)",
+        },
+        entityColor: {
+          location: "hsl(20, 67%, 97%)",
+          person: "hsl(120, 67%, 90%)",
+          institution: "hsl(160, 67%, 80%)",
+        },
+      },
+      dropShadow: {
+        top: "15px 15px 15px rgba(0, 0, 0, 0.75)",
+      },
+    },
+  },
+  plugins: [typography()],
+};
--- vite.config.local.ts
+++ vite.config.local.ts
@@ -2,24 +2,40 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindCss from "tailwindcss";
+import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: "Textannoviz",
-        },
-      },
-    }),
-    react(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
+  const tailwindConfig =
+    env.VITE_PROJECT === "republic"
+      ? "tailwind.config.republic.js"
+      : "tailwind.config.mondriaan.js";
+
+  return {
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: "Textannoviz",
+          },
+        },
+      }),
+      react(),
+    ],
+
+    css: {
+      postcss: {
+        plugins: [tailwindCss(tailwindConfig)],
+      },
+    },
+
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+    },
+  };
 });
