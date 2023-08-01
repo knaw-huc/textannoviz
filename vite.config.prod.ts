56fda2302e30391468d9104c2dc77b149b72bd0e
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-08-01 13:49:42 +0200

    Update vite prod config

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -2,32 +2,58 @@
 /// <reference types="vite/client" />
 
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import tailwindCss from "tailwindcss";
+import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-  esbuild: {
-    drop: ["console"],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: "Textannoviz",
+  let tailwindConfig: string;
+
+  switch (env.VITE_PROJECT) {
+    case "republic":
+      tailwindConfig = "tailwind.config.republic.js";
+      break;
+    case "mondriaan":
+      tailwindConfig = "tailwind.config.mondriaan.js";
+      break;
+    case "globalise":
+      tailwindConfig = "tailwind.config.globalise.js";
+      break;
+    default:
+      tailwindConfig = "tailwind.config.js";
+  }
+
+  return {
+    esbuild: {
+      drop: ["console"],
+    },
+    plugins: [
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: "Textannoviz",
+          },
         },
+      }),
+      react(),
+    ],
+
+    css: {
+      postcss: {
+        plugins: [tailwindCss(tailwindConfig)],
       },
-    }),
-    react(),
-  ],
+    },
 
-  build: {
-    target: "esnext",
-  },
+    build: {
+      target: "esnext",
+    },
 
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+    },
+  };
 });
