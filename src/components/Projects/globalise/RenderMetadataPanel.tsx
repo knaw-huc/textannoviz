15a95eebf75de57b8843705bcef2d69d7591ca75
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-09-13 14:11:27 +0200

    Add metadata panels for GeneralMissive and TextRegion annotations

--- src/components/Projects/globalise/RenderMetadataPanel.tsx
+++ src/components/Projects/globalise/RenderMetadataPanel.tsx
@@ -1,4 +1,6 @@
+import { useParams } from "react-router-dom";
 import { AnnoRepoAnnotation } from "../../../model/AnnoRepoAnnotation";
+import { useProjectStore } from "../../../stores/project";
 
 type RenderMetadataPanelProps = {
   annotations: AnnoRepoAnnotation[];
@@ -9,14 +11,74 @@ export const RenderMetadataPanel = (props: RenderMetadataPanelProps) => {
   //   (anno) => anno.body.type === "Document",
   // );
 
+  const params = useParams();
+  const projectConfig = useProjectStore((state) => state.projectConfig);
+
+  const generalMissiveAnnotation = props.annotations.filter(
+    (annotation) => annotation.body.type === "GeneralMissive",
+  );
+
+  const textRegionAnnotation = props.annotations.filter(
+    (annotation) => annotation.body.type === "px:TextRegion",
+  );
+
+  const gridOneColumn = "grid grid-cols-1";
+
+  function renderGeneralMissiveMetadata() {
+    return (
+      <ul className="m-0 list-none p-0">
+        {Object.entries(generalMissiveAnnotation[0].body.metadata).map(
+          ([key, value], i) => {
+            return (
+              <li key={i} className="mb-8 grid grid-cols-1">
+                {`${key}: ${value}`}
+              </li>
+            );
+          },
+        )}
+      </ul>
+    );
+  }
+
+  function renderTextRegionMetadata() {
+    return (
+      <ul className="m-0 list-none p-0">
+        {Object.entries(textRegionAnnotation[0].body.metadata).map(
+          ([key, value], i) => {
+            return (
+              <li key={i} className="mb-8 grid grid-cols-1">
+                {`${key}: ${value}`}
+              </li>
+            );
+          },
+        )}
+      </ul>
+    );
+  }
+
+  function renderMetadataPanelAnnotationView() {
+    if (
+      params.tier2?.includes("missive") &&
+      generalMissiveAnnotation.length > 0
+    ) {
+      return renderGeneralMissiveMetadata();
+    }
+
+    if (
+      params.tier2?.includes("textregion") &&
+      textRegionAnnotation.length > 0
+    ) {
+      return renderTextRegionMetadata();
+    }
+
+    return <div>No panel defined for this annotation type.</div>;
+  }
+
   return (
-    <ul className="m-0 list-none p-0">
-      <li className="mb-8">
-        <div className="grid grid-cols-1">
-          {/* <strong>Document: </strong>
+    <>
+      {params.tier2 ? renderMetadataPanelAnnotationView() : null}
+      {/* <strong>Document: </strong>
           {(documentAnno[0].body as DocumentBody).metadata.document} */}
-        </div>
-      </li>
-    </ul>
+    </>
   );
 };
