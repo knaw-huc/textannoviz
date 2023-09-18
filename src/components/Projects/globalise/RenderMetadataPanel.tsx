545d49c8d1bc4ba96431d83ba37772a5d0cb3adc
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-09-18 12:11:10 +0200

    Add annotation type and link to annotation in AR to metadata panel

--- src/components/Projects/globalise/RenderMetadataPanel.tsx
+++ src/components/Projects/globalise/RenderMetadataPanel.tsx
@@ -17,12 +17,18 @@ export const RenderMetadataPanel = (props: RenderMetadataPanelProps) => {
   const generalMissiveAnnotation = props.annotations.filter(
     (annotation) => annotation.body.type === "GeneralMissive",
   );
-
   const textRegionAnnotation = props.annotations.filter(
     (annotation) => annotation.body.type === "px:TextRegion",
   );
+  const pageAnnotation = props.annotations.filter(
+    (annotation) => annotation.body.type === "px:Page",
+  );
 
   const gridOneColumn = "grid grid-cols-1";
+
+  const currentAnnotation = props.annotations.filter(
+    (annotation) => annotation.body.id === params.tier2,
+  );
 
   function renderGeneralMissiveMetadata() {
     return (
@@ -43,6 +49,25 @@ export const RenderMetadataPanel = (props: RenderMetadataPanelProps) => {
   function renderTextRegionMetadata() {
     return (
       <ul className="m-0 list-none p-0">
+        <li className="mb-8">
+          <strong>Annotation type: </strong>
+          <div className={gridOneColumn}>
+            {textRegionAnnotation[0].body.type}
+          </div>
+        </li>
+        <li className="mb-8">
+          <strong>Link to AnnoRepo: </strong>
+          <div className={gridOneColumn}>
+            <a
+              href={textRegionAnnotation[0].id}
+              title="AnnoRepo link"
+              rel="noreferrer"
+              target="_blank"
+            >
+              {textRegionAnnotation[0].id}
+            </a>
+          </div>
+        </li>
         {Object.entries(textRegionAnnotation[0].body.metadata).map(
           ([key, value], i) => {
             return (
@@ -52,6 +77,28 @@ export const RenderMetadataPanel = (props: RenderMetadataPanelProps) => {
             );
           },
         )}
+      </ul>
+    );
+  }
+
+  function renderPageMetadata() {
+    return (
+      <ul className="m-0 list-none p-0">
+        <li className="mb-8">
+          <strong>Annotation type: </strong>
+          <div className={gridOneColumn}>{pageAnnotation[0].body.type}</div>
+        </li>
+        <li className="mb-8">
+          <strong>Link to AnnoRepo: </strong>
+          <a
+            href={pageAnnotation[0].id}
+            title="AnnoRepo link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {pageAnnotation[0].id}
+          </a>
+        </li>
       </ul>
     );
   }
@@ -69,6 +116,13 @@ export const RenderMetadataPanel = (props: RenderMetadataPanelProps) => {
       textRegionAnnotation.length > 0
     ) {
       return renderTextRegionMetadata();
+    }
+
+    if (
+      currentAnnotation[0].body.type === "px:Page" &&
+      pageAnnotation.length > 0
+    ) {
+      return renderPageMetadata();
     }
 
     return <div>No panel defined for this annotation type.</div>;
