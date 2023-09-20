361c7b14742a9ebd1f7bd8e63fa2cdfa92d767bf
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-09-20 10:30:55 +0200

    Re-enable sidebar in Mirador

--- src/components/Mirador/MiradorConfig.ts
+++ src/components/Mirador/MiradorConfig.ts
@@ -4,7 +4,7 @@ export const miradorConfig: MiradorConfig = {
   id: "mirador",
   window: {
     allowClose: false,
-    allowWindowSideBar: false,
+    allowWindowSideBar: true,
     allowFullscreen: false,
     highlightAllAnnotations: false, //this set to 'true' will always highlight all annotations, handy for debugging
     forceDrawAnnotations: true, //this should be 'true' for 'selectAnnotation' to render the selected annotation. Without this, the selected annotation will not be rendered with the API call
