f7baf78293935fe4ad1d8aa455d8c50b07182da2
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-09-28 09:00:14 +0200

    Moved logo to the left in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,12 @@ export const Header = (props: HeaderProps) => {
     <header className="border-brand1-200 text-brand1-700 border-b">
       <div className="mx-auto w-full px-6">
         <div className="flex flex-row items-center justify-start">
-          <div className="bg-brand1-100 text-brand1-800 -mx-6 -my-3 flex flex-row items-center justify-start gap-3 py-3 pl-3">
+          <div className="bg-brand1-100 text-brand1-800 -mx-6 -my-3 flex flex-row items-center justify-start gap-3 py-3 pr-3">
+            <div className="bg-brand2-200 flex h-12 w-12 items-center justify-center">
+              <a title="Homepage" rel="noreferrer" href="/search">
+                <img src={logoUrl} className="h-7 w-7" />
+              </a>
+            </div>
             <span className="uppercase">
               <a
                 title="Homepage"
@@ -20,11 +25,6 @@ export const Header = (props: HeaderProps) => {
                 {props.project}
               </a>
             </span>
-            <div className="bg-brand2-200 flex h-12 w-12 items-center justify-center">
-              <a title="Homepage" rel="noreferrer" href="/search">
-                <img src={logoUrl} className="h-7 w-7" />
-              </a>
-            </div>
           </div>
         </div>
       </div>
