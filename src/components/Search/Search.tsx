e707fca2590f7a1c7ce8cbc231c3342ca148e643
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-10-03 11:20:32 +0200

    Change link 'new search query' from /search to /

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -570,7 +570,7 @@ export const Search = (props: SearchProps) => {
         {searchResults ? (
           <div className="w-full max-w-[450px]">
             <Link
-              to="/search"
+              to="/"
               reloadDocument
               className="bg-brand2-100 text-brand2-700 hover:text-brand2-900 disabled:bg-brand2-50 active:bg-brand2-200 disabled:text-brand2-200 rounded px-2 py-2 text-sm no-underline"
             >
