22a4129438fcb50259ba22bbdb04194ae4188b37
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-10-23 12:03:20 +0200

    Removed vite-html-plugin + added TS checker to vite.config.prod.ts

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -4,46 +4,32 @@
 import react from "@vitejs/plugin-react";
 import tailwindCss from "tailwindcss";
 import { defineConfig, loadEnv } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
+import checker from "vite-plugin-checker";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   let tailwindConfig: string;
-  let title: string;
 
   switch (env.VITE_PROJECT) {
     case "republic":
       tailwindConfig = "tailwind.config.republic.js";
-      title = "Textannoviz - Republic";
       break;
     case "mondriaan":
       tailwindConfig = "tailwind.config.mondriaan.js";
-      title = "Textannoviz - Mondriaan";
       break;
     case "globalise":
       tailwindConfig = "tailwind.config.globalise.js";
-      title = "Globalise Transcriptions Viewer";
       break;
     default:
       tailwindConfig = "tailwind.config.js";
-      title = "Textannoviz";
   }
 
   return {
     esbuild: {
       drop: ["console"],
     },
-    plugins: [
-      createHtmlPlugin({
-        inject: {
-          data: {
-            title: title,
-          },
-        },
-      }),
-      react(),
-    ],
+    plugins: [react(), checker({ typescript: true })],
 
     css: {
       postcss: {
