8758916885c6de552f00b5b7551f54f53b22c231
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-11-01 10:29:51 +0100

    Fix for overflow bug in metadata panel

--- src/components/Annotations/annotation.tsx
+++ src/components/Annotations/annotation.tsx
@@ -7,7 +7,11 @@ import styled from "styled-components";
 import { ProjectConfig } from "../../model/ProjectConfig";
 import { useAnnotationStore } from "../../stores/annotation";
 import { useMiradorStore } from "../../stores/mirador";
-import {projectConfigSelector, translateSelector, useProjectStore} from "../../stores/project";
+import {
+  projectConfigSelector,
+  translateSelector,
+  useProjectStore,
+} from "../../stores/project";
 import { visualizeAnnosMirador } from "../../utils/visualizeAnnosMirador";
 import { AnnotationFilter } from "./AnnotationFilter";
 import { AnnotationItem } from "./AnnotationItem";
@@ -56,7 +60,7 @@ export function Annotation(props: AnnotationProps) {
 
   return (
     <div className="border-brand1Grey-100 relative hidden w-2/12 grow self-stretch border-x md:block">
-      <Tabs className="flex h-[calc(100vh-79px)] flex-col overflow-hidden">
+      <Tabs className="flex h-[calc(100vh-79px)] flex-col overflow-auto">
         <TabList
           aria-label="annotation-panel"
           className="border-brand1Grey-100 sticky top-0 flex w-full border-b bg-white text-sm text-neutral-600"
@@ -65,20 +69,18 @@ export function Annotation(props: AnnotationProps) {
             id="metadata"
             className="aria-selected:bg-brand1Grey-100 hover:bg-brand1Grey-50 px-4 py-2 outline-none transition-colors duration-200 hover:cursor-pointer"
           >
-            {translate('METADATA')}
+            {translate("METADATA")}
           </Tab>
           <Tab
             id="webannos"
             className="aria-selected:bg-brand1Grey-100 hover:bg-brand1Grey-50 px-4 py-2 outline-none transition-colors duration-200 hover:cursor-pointer"
           >
-            {translate('WEB_ANNOTATIONS')}
+            {translate("WEB_ANNOTATIONS")}
           </Tab>
         </TabList>
         <TabPanel id="metadata" className="text-brand1-800 h-full p-5">
           {annotations.length > 0 && !props.isLoading ? (
-            <projectConfig.components.MetadataPanel
-                annotations={annotations}
-            />
+            <projectConfig.components.MetadataPanel annotations={annotations} />
           ) : (
             <div>
               <Skeleton width="25rem" className="skeleton"></Skeleton>
