27a7049c247bf1b3a9c67896e4f3b5f67fe9fb23
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-11-08 16:57:50 +0100

    Make Mirador wider

--- src/components/Mirador/Mirador.tsx
+++ src/components/Mirador/Mirador.tsx
@@ -2,7 +2,7 @@ export function Mirador() {
   return (
     <div
       id="mirador"
-      className="bg-brand1Grey-50 sticky top-0 hidden h-[calc(100vh-79px)] w-4/12 max-w-2xl grow self-stretch overflow-auto lg:flex"
+      className="bg-brand1Grey-50 sticky top-0 hidden h-[calc(100vh-79px)] w-7/12 grow self-stretch overflow-auto lg:flex"
     ></div>
   );
 }
