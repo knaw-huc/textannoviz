6788f4e8b85bbbcac7b08c99daf9c2505d3ee5ef
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-12-12 08:34:33 +0100

    Updated shell scripts

--- docker-republic-all.sh
+++ docker-republic-all.sh
@@ -3,8 +3,10 @@
 VERSION=$(npm pkg get version | tr -d \")
 DOCKER_DOMAIN=registry.diginfra.net/tt
 PROJECT=republic
+TITLE="Textannoviz - Republic"
 
 find .env -type f -exec sed -i '' -e "/^VITE_PROJECT=/s/=.*/=$PROJECT/" {} \;
+find .env -type f -exec sed -i '' -e "/^VITE_TITLE=/s/=.*/=$TITLE/" {} \;
 
 #Uncomment this line if running this script with the GNU version of sed. See: https://riptutorial.com/sed/topic/9436/bsd-macos-sed-vs--gnu-sed-vs--the-posix-sed-specification
 #find .env -type f -exec sed -i "/^PROJECT=/s/=.*/=$PROJECT/" {} \;
