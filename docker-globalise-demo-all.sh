9143ae35410d9faf9147d2d75ef0d3476648556c
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2023-12-31 15:57:23 +0100

    Explicitly state build platform Docker build scripts due to transition to Apple Silicon

--- docker-globalise-demo-all.sh
+++ docker-globalise-demo-all.sh
@@ -11,7 +11,7 @@ find .env -type f -exec sed -i '' -e "/^VITE_TITLE=/s/=.*/=$TITLE/" {} \;
 #Uncomment this line if running this script with the GNU version of sed. See: https://riptutorial.com/sed/topic/9436/bsd-macos-sed-vs--gnu-sed-vs--the-posix-sed-specification
 #find .env -type f -exec sed -i "/^PROJECT=/s/=.*/=$PROJECT/" {} \;
 
-docker build -t textannoviz-${PROJECT}-demo-frontend:${VERSION} -f deploy/Dockerfile-deploy .
+docker build -t textannoviz-${PROJECT}-demo-frontend:${VERSION} --platform=linux/amd64 -f deploy/Dockerfile-deploy .
 
 docker tag textannoviz-${PROJECT}-demo-frontend:${VERSION} ${DOCKER_DOMAIN}/textannoviz-${PROJECT}-demo-frontend:${VERSION}
 
