900095bba6eefd4a76fd7d134ada0d5ea12757fe
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-02-20 12:27:54 +0100

    Suriano translation clarification

--- src/projects/suriano/config/dutchSurianoLabels.ts
+++ src/projects/suriano/config/dutchSurianoLabels.ts
@@ -4,7 +4,7 @@ export const dutchSurianoLabels = Object.assign({}, dutchLabels, {
   self: "Tekst",
 
   // search facet titles:
-  bodyType: "Document",
+  bodyType: "Document type",
   "tf:File": "Letter",
   senderLoc: "Locatie van zender",
   sender: "Zender",
