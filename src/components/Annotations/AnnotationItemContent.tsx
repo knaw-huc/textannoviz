9f3df2c4220155263b04bd024de90bd08354c7e0
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-04-17 14:04:00 +0200

    HTML button to react-aria Button

--- src/components/Annotations/AnnotationItemContent.tsx
+++ src/components/Annotations/AnnotationItemContent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-aria-components";
 import { AnnoRepoAnnotation } from "../../model/AnnoRepoAnnotation";
 import { projectConfigSelector, useProjectStore } from "../../stores/project";
 
@@ -17,14 +18,15 @@ export function AnnotationItemContent(props: AnnotationContentProps) {
           annotation={props.annotation}
         />
         <li>
-          <button
-            onClick={() => {
+          <Button
+            className="outline-none"
+            onPress={() => {
               setShowFull(!showFull);
             }}
           >
             <strong>full annotation</strong>{" "}
             {String.fromCharCode(showFull ? 9663 : 9657)}
-          </button>
+          </Button>
           <div className="overflow-auto">
             {showFull && <pre>{JSON.stringify(props.annotation, null, 2)}</pre>}
           </div>
