bb8ee1b6218b493afd3cbfe42112d05fe3e9e502
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-05-07 13:58:54 +0200

    Updated TextFieldComponent

--- src/components/common/TextFieldComponent.tsx
+++ src/components/common/TextFieldComponent.tsx
@@ -7,16 +7,26 @@ import {
 
 interface TextFieldComponentProps extends TextFieldProps {
   label?: string;
+  width?: string;
+  labelStyling?: string;
 }
 
 export function TextFieldComponent({
   label,
+  labelStyling,
+  width,
   ...props
 }: TextFieldComponentProps) {
   return (
-    <TextField {...props}>
-      {label && <Label>{label}</Label>}
-      <Input className="border-brand1Grey-700 mr-2 w-16 rounded border bg-white px-2 py-1 text-sm" />
+    <TextField {...props} className="flex flex-col gap-1">
+      {label && (
+        <Label className={`${labelStyling ? labelStyling : ""}`}>{label}</Label>
+      )}
+      <Input
+        className={` ${
+          width ? width : "w-16"
+        } border-brand1Grey-700 mr-2 rounded-md border bg-white px-2 py-1 text-sm`}
+      />
     </TextField>
   );
 }
