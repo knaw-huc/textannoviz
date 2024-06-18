263d903b5328398afbbc4a921a4a6c26c8626d78
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-06-18 10:49:53 +0200

    Self-closing div Mirador

--- src/components/Mirador/Mirador.tsx
+++ src/components/Mirador/Mirador.tsx
@@ -137,6 +137,6 @@ export function Mirador(props: MiradorProps) {
     <div
       id="mirador"
       className="bg-brand1Grey-50 sticky top-0 hidden h-[calc(100vh-79px)] w-7/12 grow self-stretch overflow-auto lg:flex"
-    ></div>
+    />
   );
 }
