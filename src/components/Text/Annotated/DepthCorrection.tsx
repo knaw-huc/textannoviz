5938f36bf7946543d74ec2ee54d76726418f3e5a
BasLee <basleenknegt@gmail.com>
2024-06-19 14:46:26 +0200

    Move comment on aligning underlines between segments to DepthCorrection

--- src/components/Text/Annotated/DepthCorrection.tsx
+++ src/components/Text/Annotated/DepthCorrection.tsx
@@ -1,5 +1,22 @@
 import { PropsWithChildren } from "react";
 
+/**
+ * Every annotation has a certain depth. In order to align underlines between segments,
+ * gaps between annotations and the text or other annotations need to be filled.
+ *
+ * Consider three annotations, ABC, AB and BC:
+ *          text: aabbcc
+ * BC  (depth 3): ##____
+ * AB  (depth 2): ____##
+ * ABC (depth 1): ______
+ * (underscores mark an underlining, hashes mark empty space)
+ *
+ * The annotation BC overlaps with AB.
+ * This overlap results in two empty parts:
+ * - an empty spot between AB and the text `aa`;
+ * - an empty 'lane' between BC and the parent annotation ABC.
+ * To render these empty spots, a spacing component `DepthCorrection` is added.
+ */
 export function DepthCorrection(
   props: PropsWithChildren<{
     depthCorrection: number;
