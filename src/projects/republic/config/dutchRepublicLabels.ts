317d71ad2d7747ba6d4421ca68f88035983e2b63
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-06-20 10:53:18 +0200

    Removed obsolete Dutch weekday translations

--- src/projects/republic/config/dutchRepublicLabels.ts
+++ src/projects/republic/config/dutchRepublicLabels.ts
@@ -2,13 +2,6 @@ import { dutchLabels } from "../../default/config/dutchLabels.ts";
 
 export const dutchRepublicLabels = Object.assign({}, dutchLabels, {
   // facet translations:
-  Lunæ: "Maandag",
-  Martis: "Dinsdag",
-  Mercurii: "Woensdag",
-  Jovis: "Donderdag",
-  Veneris: "Vrijdag",
-  Sabbathi: "Zaterdag",
-  Dominica: "Zondag",
   Resolution: "Resolutie",
   AttendanceList: "Presentielijst",
 
