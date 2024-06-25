b4e5b20e81b8b09efc31b9c25b52734532f0c38a
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-06-25 10:43:02 +0200

    Added plus and minus icons to Show more and Show less

--- src/components/Search/ShowMoreButton.tsx
+++ src/components/Search/ShowMoreButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "react-aria-components";
+import { PlusIcon } from "../common/icons/PlusIcon";
 
 type ShowMoreButtonProps = {
   showMoreButtonClickHandler: (aggregation: string) => void;
@@ -8,10 +9,13 @@ type ShowMoreButtonProps = {
 export function ShowMoreButton(props: ShowMoreButtonProps) {
   return (
     <Button
-      className="text-brand2-500 hover:text-brand2-700 border-brand2-100 bg-brand2-50 -mt-10 flex h-10 w-full max-w-[450px] items-center justify-end border-t-2 pr-2 outline-none"
+      className="text-brand2-500 fill-brand2-500 hover:text-brand2-700 hover:fill-brand2-700 border-brand2-100 bg-brand2-50 -mt-10 flex h-10 w-full max-w-[450px] items-center justify-end border-t-2 pr-2 outline-none"
       onPress={() => props.showMoreButtonClickHandler(props.facetName)}
     >
-      Show more +
+      Show more{" "}
+      <span className="pl-1">
+        <PlusIcon />
+      </span>
     </Button>
   );
 }
