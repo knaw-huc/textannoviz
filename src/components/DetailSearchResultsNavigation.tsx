6bb68ac6bb69a818beff483e07dceb241d43f4c8
BasLee <basleenknegt@gmail.com>
2024-07-11 13:25:19 +0200

    Load next search result page when browsing detail page

--- src/components/DetailSearchResultsNavigation.tsx
+++ src/components/DetailSearchResultsNavigation.tsx
@@ -1,24 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import { translateSelector, useProjectStore } from "../stores/project.ts";
 import { useSearchStore } from "../stores/search/search-store.ts";
-import {
-  DetailParams,
-  useDetailUrlParams,
-} from "./Text/Annotated/utils/useDetailUrlParams.tsx";
+import { useDetailUrlParams } from "./Text/Annotated/utils/useDetailUrlParams.tsx";
 import { Base64 } from "js-base64";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { SearchResult } from "../model/Search.ts";
 import { toDetailPageUrl } from "./Text/Annotated/utils/toDetailPageUrl.tsx";
 import { FooterLink } from "./FooterLink.tsx";
+import { usePagination } from "../utils/usePagination.tsx";
+import { useSearchResults } from "./Search/useSearchResults.tsx";
 
 export function DetailSearchResultsNavigation() {
   const navigate = useNavigate();
   const translate = useProjectStore(translateSelector);
-  const { searchQuery, searchUrlParams, searchResults } = useSearchStore();
   const detailParams = useDetailUrlParams();
 
-  const { prevResultPath, nextResultPath } = createPrevNextUrls(
-    detailParams,
+  const {
+    searchQuery,
+    searchUrlParams,
+    searchResults,
+    searchFacetTypes,
+    setSearchResults,
+  } = useSearchStore();
+  const { selectNextPage, hasNextPage } = usePagination();
+  const { getSearchResults } = useSearchResults();
+
+  const prevResultPath = createPrevUrl(
+    detailParams.tier2,
+    detailParams.highlight,
+    searchResults,
+  );
+  const nextResultPath = createNextUrl(
+    detailParams.tier2,
+    detailParams.highlight,
     searchResults,
   );
 
@@ -27,38 +41,53 @@ export function DetailSearchResultsNavigation() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const urlSearchParams = new URLSearchParams(searchUrlParams as any);
 
+  const total = searchResults?.total.value || 0;
+
   const isOnFirstOfPage = !prevResultPath;
   // TODO:
   const hasPreviousPage = false;
   const isPrevDisabled = isOnFirstOfPage && !hasPreviousPage;
 
   const isOnEndOfPage = !nextResultPath;
-  const total = searchResults?.total.value || 0;
   const lastPageResult = searchUrlParams.from + searchUrlParams.size;
-  // TODO:
-  const hasNextPage = total > lastPageResult;
-  const isNextDisabled = isOnEndOfPage && !hasNextPage;
+  const isNextDisabled = isOnEndOfPage && !hasNextPage();
+
   console.log("DetailSearchResultsNavigation", {
     total,
     isOnEndOfPage,
     lastPageResult,
-    hasNextPage,
+    hasNextPage: hasNextPage(),
     isNextDisabled,
   });
 
-  function handleNextPageClick() {
-    if (!nextResultPath && !hasNextPage) {
+  async function handleNextResultClick() {
+    if (!nextResultPath && !hasNextPage()) {
       return;
     }
+
     if (nextResultPath) {
       navigate(nextResultPath);
       return;
     }
-    console.log(`
-    // TODO:
-    //  - load next page
-    //  - navigate to first result on next page
-    `);
+
+    selectNextPage();
+    const newSearchResults = await getSearchResults(
+      searchFacetTypes,
+      searchUrlParams,
+      searchQuery,
+    );
+    if (newSearchResults) {
+      const nextUrl = createNextUrl(
+        newSearchResults.results.results[0]._id,
+        detailParams.highlight,
+        newSearchResults.results,
+      );
+      if (!nextUrl) {
+        throw new Error("No results found");
+      }
+      navigate(nextUrl);
+      setSearchResults(newSearchResults.results);
+    }
   }
 
   return (
@@ -78,38 +107,49 @@ export function DetailSearchResultsNavigation() {
         <MagnifyingGlassIcon className="inline h-4 w-4 fill-neutral-500" />{" "}
         {translate("BACK_TO_SEARCH")}
       </FooterLink>
-      <FooterLink onClick={handleNextPageClick} disabled={isNextDisabled}>
+      <FooterLink onClick={handleNextResultClick} disabled={isNextDisabled}>
         Next &gt;
       </FooterLink>
     </>
   );
 }
 
-function createPrevNextUrls(
-  detailParams: DetailParams,
+function createPrevUrl(
+  tier2: string,
+  highlight: string | undefined,
   searchResults?: SearchResult,
-): {
-  prevResultPath?: string;
-  nextResultPath?: string;
-} {
+): string | undefined {
   if (!searchResults) {
-    return {};
+    return;
   }
-  const { tier2, highlight } = detailParams;
   const resultIndex = searchResults.results.findIndex((r) => r._id === tier2);
-  const prevResultId =
-    resultIndex > 0 ? searchResults.results[resultIndex - 1]._id : undefined;
-  const prevResultPath = prevResultId
-    ? toDetailPageUrl(prevResultId, { highlight })
-    : undefined;
-  const nextResultId =
-    resultIndex !== -1 && resultIndex < searchResults.results.length - 1
-      ? searchResults.results[resultIndex + 1]._id
-      : undefined;
-  const nextResultPath = nextResultId
-    ? toDetailPageUrl(nextResultId, { highlight })
-    : undefined;
-  return { prevResultPath, nextResultPath };
+  if (resultIndex === -1) {
+    return;
+  }
+  const prevResultId = searchResults.results[resultIndex - 1]?._id;
+  if (!prevResultId) {
+    return;
+  }
+  return toDetailPageUrl(prevResultId, { highlight });
+}
+
+function createNextUrl(
+  tier2: string,
+  highlight: string | undefined,
+  searchResults?: SearchResult,
+): string | undefined {
+  if (!searchResults) {
+    return;
+  }
+  const resultIndex = searchResults.results.findIndex((r) => r._id === tier2);
+  if (resultIndex === -1) {
+    return;
+  }
+  const nextResultId = searchResults.results[resultIndex + 1]?._id;
+  if (!nextResultId) {
+    return;
+  }
+  return toDetailPageUrl(nextResultId, { highlight });
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
