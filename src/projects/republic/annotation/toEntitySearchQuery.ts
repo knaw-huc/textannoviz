d25df1eb2dcf6cbfabc3c05a8386e615dee5b08e
BasLee <basleenknegt@gmail.com>
2024-09-26 10:51:44 +0200

    Add todo to republic toEntitySearchQuery

--- src/projects/republic/annotation/toEntitySearchQuery.ts
+++ src/projects/republic/annotation/toEntitySearchQuery.ts
@@ -14,6 +14,7 @@ export function toEntitySearchQuery(
   if (isDateEntity(anno)) {
     return toDateSearchQuery(anno.metadata.date, searchStore);
   } else {
+    // TODO:
     return false;
   }
 }
