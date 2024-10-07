1d0ccfea29ec6183d1988b2ce41993d84f2d26b9
BasLee <basleenknegt@gmail.com>
2024-10-07 10:21:03 +0200

    Remove broccoli dummy entity with page break

--- src/utils/broccoli.ts
+++ src/utils/broccoli.ts
@@ -2,7 +2,6 @@ import { toast } from "react-toastify";
 import { ProjectConfig } from "../model/ProjectConfig";
 import { SearchQueryRequestBody, SearchResult } from "../model/Search";
 import { SearchUrlParams } from "../stores/search/search-params-slice.ts";
-import { dummyAnno, dummyViewAnno } from "./broccoliDummyAnno.ts";
 
 const headers = {
   "Content-Type": "application/json",
@@ -17,26 +16,14 @@ export const fetchBroccoliScanWithOverlap = async (
   config: ProjectConfig,
   signal: AbortSignal,
 ) => {
-  const url =
-    `${config.broccoliUrl}/projects/${config.id}/${bodyId}` +
-    `?overlapTypes=${overlapTypes}` +
-    `&includeResults=${includeResults}` +
-    `&views=${views}` +
-    `&relativeTo=${relativeTo}`;
+  const url = `${config.broccoliUrl}/projects/${config.id}/${bodyId}?overlapTypes=${overlapTypes}&includeResults=${includeResults}&views=${views}&relativeTo=${relativeTo}`;
   const response = await fetch(url, { signal });
   if (!response.ok) {
     const error = await response.json();
     toast(`${error.message}`, { type: "error" });
     return null;
   }
-  const result = await response.json();
-  // TODO: remove test data
-  if (bodyId.includes("1807474") && overlapTypes.includes("tf:Ent")) {
-    console.log("dummy result", result);
-    result.anno.push(dummyAnno);
-    result.views.self.locations.annotations.push(dummyViewAnno);
-  }
-  return result;
+  return await response.json();
 };
 
 export const selectDistinctBodyTypes = async (
