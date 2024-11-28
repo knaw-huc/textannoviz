ba3c4d3115034f3ea1bb6c6054e36fe93899c640
BasLee <basleenknegt@gmail.com>
2024-11-28 16:15:17 +0100

    Open help link in new tab

--- src/projects/republic/HelpLink.tsx
+++ src/projects/republic/HelpLink.tsx
@@ -3,6 +3,7 @@ export function HelpLink() {
     <div className="mr-4 flex grow flex-row items-center justify-end gap-2">
       <a
         rel="noreferrer"
+        target="_blank"
         // TODO: verify url:
         href="https://goetgevonden.nl/index.php/help/gebruik-van-de-applicatie"
         className="hover:text-brand1-100 text-inherit no-underline hover:underline"
