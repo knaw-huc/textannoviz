239bc4a838fb65645739f637ad68c5f9ce6eb17d
BasLee <basleenknegt@gmail.com>
2024-12-02 15:15:23 +0100

    Remove stacktrace from useEffect abort handler debug msg

--- src/utils/handleAbort.tsx
+++ src/utils/handleAbort.tsx
@@ -1,6 +1,6 @@
 export function handleAbort(e: Error) {
   if (e instanceof DOMException && e.name == "AbortError") {
-    console.debug("useEffect cleanup aborted request:", e);
+    console.debug("useEffect cleanup aborted request");
   } else {
     throw e;
   }
