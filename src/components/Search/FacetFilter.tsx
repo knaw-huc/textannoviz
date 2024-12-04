d1c1e4cad4b56c982d638ede933a608adb2ec7ce
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2024-12-04 14:07:18 +0100

    Align styling of "Filter facets" button

--- src/components/Search/FacetFilter.tsx
+++ src/components/Search/FacetFilter.tsx
@@ -12,9 +12,9 @@ import {
   useProjectStore,
 } from "../../stores/project";
 import { FacetEntry } from "../../stores/search/search-query-slice";
+import { HelpTooltip } from "../common/HelpTooltip.tsx";
 import { ChevronDown } from "../common/icons/ChevronDown";
 import { ChevronRight } from "../common/icons/ChevronRight";
-import { HelpTooltip } from "../common/HelpTooltip.tsx";
 
 type FacetFilterProps = {
   allPossibleKeywordFacets: FacetEntry[];
@@ -42,7 +42,7 @@ export function FacetFilter(props: FacetFilterProps) {
   return (
     <>
       <Button
-        className="bg-brand1-100 hover:bg-brand1-200 pressed:bg-brand1-300 text-brand1-700 hover:text-brand1-900 fill-brand1-700 hover:fill-brand1-900 flex w-fit flex-row items-center rounded px-2 py-2 text-sm outline-none"
+        className="bg-brand1-100 hover:bg-brand1-200 pressed:bg-brand1-300 text-brand1-900 hover:text-brand1-700 fill-brand1-900 hover:fill-brand1-700 flex w-fit flex-row items-center rounded px-2 py-2 text-sm outline-none"
         onPress={() => setIsOpen(!isOpen)}
       >
         {translate("FILTER_FACETS")}{" "}
