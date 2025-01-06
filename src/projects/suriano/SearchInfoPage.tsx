8ecac1017f57002f12e12fe4134595e7ca4a4e8d
BasLee <basleenknegt@gmail.com>
2025-01-06 12:15:34 +0100

    Add border between intro page and search result navigation

--- src/projects/suriano/SearchInfoPage.tsx
+++ src/projects/suriano/SearchInfoPage.tsx
@@ -1,14 +1,16 @@
 export const SearchInfoPage = () => {
   return (
-    <p className="mb-4 mt-4 block" style={{ maxWidth: "50em" }}>
-      Christofforo Suriano was the first Venetian envoy to the Dutch Republic.
-      His arrival in The Hague marks the beginning of official Dutch-Venetian
-      diplomatic relations. This digital edition presents all 725 letters sent
-      by Suriano to the Venetian Senate between 1616 and 1623, totaling
-      approximately 7,000 pages. His correspondence is of great importance for
-      Dutch, Venetian, and European history in the first phase of the Thirty
-      Years’ War. For more information about the edition, see:{" "}
-      <a href="https://suriano.huygens.knaw.nl/">suriano.huygens.knaw.nl</a>
-    </p>
+    <div className="border-brand1Grey-100 -mx-10 mb-20 flex flex-row border-b px-10 pb-8">
+      <p className="mb-10 mt-4 block" style={{ maxWidth: "50em" }}>
+        Christofforo Suriano was the first Venetian envoy to the Dutch Republic.
+        His arrival in The Hague marks the beginning of official Dutch-Venetian
+        diplomatic relations. This digital edition presents all 725 letters sent
+        by Suriano to the Venetian Senate between 1616 and 1623, totaling
+        approximately 7,000 pages. His correspondence is of great importance for
+        Dutch, Venetian, and European history in the first phase of the Thirty
+        Years’ War. For more information about the edition, see:{" "}
+        <a href="https://suriano.huygens.knaw.nl/">suriano.huygens.knaw.nl</a>
+      </p>
+    </div>
   );
 };
