dc62fb8315ac715c96e1e998aba2c6fa7a87a4b7
BasLee <basleenknegt@gmail.com>
2025-01-09 13:39:28 +0100

    Add todo on removing complicated url param function

--- src/components/Text/Annotated/utils/getFullDetailUrl.tsx
+++ src/components/Text/Annotated/utils/getFullDetailUrl.tsx
@@ -1,6 +1,8 @@
 import { SearchParams, SearchQuery } from "../../../../model/Search.ts";
 import { createUrlParams } from "../../../../utils/UrlParamUtils.ts";
 
+// TODO: Get rid of this function:
+//  detail nav hook does not need to know about search params and query
 export function createUrlSearchParams(
   searchParams: SearchParams,
   searchQuery: SearchQuery,
