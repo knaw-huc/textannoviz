573881ff85602c0a16677f3380e0d2594b2fdf18
BasLee <basleenknegt@gmail.com>
2025-01-09 13:48:20 +0100

    Remove searchQuery.dateFacet check

--- src/components/Search/SearchResults.tsx
+++ src/components/Search/SearchResults.tsx
@@ -160,9 +160,8 @@ export function SearchResults(props: SearchResultsProps) {
             : translate("NO_SEARCH_RESULTS")}
         </span>
         <div className="flex items-center justify-between gap-10">
-          {searchResults.results.length &&
-            projectConfig.showSearchSortBy &&
-            searchQuery.dateFacet && (
+          {searchResults.results.length >= 1 &&
+            projectConfig.showSearchSortBy && (
               <SearchSorting
                 dateFacet={searchQuery.dateFacet}
                 onSort={updateSorting}
