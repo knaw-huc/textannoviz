6205ea613f6234cfd3a16cff302e2450be23c6ce
BasLee <basleenknegt@gmail.com>
2025-02-24 10:41:49 +0100

    Use query.fullText instead of dedicated highlight url param

--- src/projects/vangogh/SearchItem.tsx
+++ src/projects/vangogh/SearchItem.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { VanGoghSearchResultsBody } from "../../model/Search";
 
 import { SearchItemProps } from "../../model/SearchItemProps.ts";
+import { encodeObject } from "../../utils/UrlParamUtils.ts";
+import _ from "lodash";
 
 export const SearchItem = (
   props: SearchItemProps<VanGoghSearchResultsBody>,
@@ -22,7 +24,9 @@ export const SearchItem = (
   return (
     <ul className="border-brand1Grey-200 mb-4 border-b">
       <Link
-        to={`/detail/${props.result._id}?highlight=${props.query.fullText}`}
+        to={`/detail/${props.result._id}?query=${encodeObject(
+          _.pick(props.query, "fullText"),
+        )}`}
         className="hover:text-brand1-600 active:text-brand1-700 text-inherit no-underline"
       >
         <li className="divide-brand1Grey-100 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-6 w-full cursor-pointer divide-y divide-solid rounded border bg-white shadow-sm transition hover:bg-white">
