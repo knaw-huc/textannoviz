98e9c3af866fcfa3bb75bcb45edbaf90b3a328eb
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-02-26 15:55:50 +0100

    Iterate over panels list to render multiple panels

--- src/components/Detail/Panels.tsx
+++ src/components/Detail/Panels.tsx
@@ -3,21 +3,38 @@ import { Panel } from "./Panel";
 import { TextComponentTab } from "./TextComponentTab";
 
 export const Panels = () => {
-  const tabs = [
+  const panels = [
     {
-      id: 1,
-      title: "Facsimile",
-      content: <MiradorTab />,
+      name: "facs-text",
+      tabs: [
+        {
+          id: 1,
+          title: "Facsimile",
+          content: <MiradorTab />,
+        },
+        {
+          id: 2,
+          title: "Text",
+          content: <TextComponentTab />,
+        },
+      ],
     },
     {
-      id: 2,
-      title: "Text",
-      content: <TextComponentTab />,
+      name: "text",
+      tabs: [
+        {
+          id: 3,
+          title: "Text",
+          content: <TextComponentTab />,
+        },
+      ],
     },
   ];
   return (
     <>
-      <Panel tabsToRender={tabs} />
+      {panels.map((panel, index) => (
+        <Panel key={index} tabsToRender={panel.tabs} />
+      ))}
     </>
   );
 };
