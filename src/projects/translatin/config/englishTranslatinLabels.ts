433ece216f5baae82805e85583c247592ced9934
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-04-10 15:03:23 +0200

    Added Translatin Search info page + first English labels

---
+++ src/projects/translatin/config/englishTranslatinLabels.ts
@@ -0,0 +1,12 @@
+import { englishLabels } from "../../default/config/englishLabels";
+
+export const englishTranslatinLabels = Object.assign({}, englishLabels, {
+  //Search facets
+  author: "Author",
+  titleShort: "Play title",
+  genre: "Genre",
+  firstEdition: "Year of first publication",
+  pubYear: "Year of publication",
+  pubPlace: "Place of publication",
+  publisher: "Publisher",
+});
