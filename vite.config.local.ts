9e9f255830b2cae6f77c5899c64b3b511455251e
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-05-06 14:03:00 +0200

    Move typescript checker overlay to top-right so it doesn't overlap with the footer

--- vite.config.local.ts
+++ vite.config.local.ts
@@ -55,6 +55,7 @@ export default defineConfig(({ mode }) => {
         typescript: true,
         overlay: {
           initialIsOpen: false,
+          position: "tr",
         },
       }),
       htmlInjectionPluginConfig &&
