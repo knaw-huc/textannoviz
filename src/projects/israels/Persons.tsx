2de47d2995a8f4736a9168c22d2afeae1b3fe196
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-06-03 08:32:26 +0200

    Enhance Persons component: add HelpIcon for source links and improve layout for display label and action icons

--- src/projects/israels/Persons.tsx
+++ src/projects/israels/Persons.tsx
@@ -3,10 +3,12 @@ import { toast } from "react-toastify";
 import { SearchQuery } from "../../model/Search";
 import { encodeObject } from "../../utils/UrlParamUtils";
 import { MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import { HelpIcon } from "../../components/common/icons/HelpIcon";
 
 type Person = {
   id: string;
   sex: string;
+  source?: string;
   persName: {
     full: string;
     forename: string;
@@ -39,12 +41,14 @@ export function Persons() {
     async function initPersons() {
       const newPersons = await fetchPersons();
       if (!newPersons) return;
+
       newPersons.sort((a, b) =>
         a.displayLabel.localeCompare(b.displayLabel, "en", {
           sensitivity: "base",
           ignorePunctuation: true,
         }),
       );
+
       setPersons(newPersons);
     }
 
@@ -62,27 +66,46 @@ export function Persons() {
     window.open(`/?${encodedQuery}`, "_blank");
   }
 
+  function moreInfoPerson(link: string) {
+    //Some links look like "https://rkd.nl/artists/108502 http://vocab.getty.edu/ulan/500212765 https://www.wikidata.org/wiki/Q16867", so those are split on whitespace and then the first is opened in a new window
+    const splitLink = link.split(" ");
+    window.open(splitLink[0], "_blank");
+  }
+
   return (
     <div
       style={{
-        display: "grid",
         gridTemplateColumns: "repeat(auto-fit,minmax(320px,1fr))",
-        gridGap: "1.5rem",
       }}
-      className="p-2"
+      className="grid gap-6 p-2"
     >
       {persons?.map((per, index) => (
-        <div className="max-w-[800px] rounded bg-neutral-50 p-5" key={index}>
-          <div className="flex flex-row items-center justify-between">
-            <div className="w-fit font-bold">{per.displayLabel}</div>
+        <div
+          className="h-36 max-w-[800px] rounded bg-neutral-50 p-5"
+          key={index}
+        >
+          <div className="flex flex-row items-center">
+            <div className="flex w-fit flex-grow flex-row items-center justify-start font-bold">
+              {per.displayLabel}
+            </div>
             {/* TODO: SVG wordt kleiner wanneer `displayLabel` langer is dan 1 regel */}
-            <MagnifyingGlassIcon
-              aria-hidden
-              className="h-4 w-4 cursor-pointer text-gray-500"
-              onClick={() => searchPerson(per)}
-            />
-          </div>
+            <div className="flex flex-row items-center justify-end gap-1">
+              {per.source ? (
+                <span
+                  className="flex cursor-pointer items-center"
+                  onClick={() => moreInfoPerson(per.source!)}
+                >
+                  <HelpIcon />
+                </span>
+              ) : null}
 
+              <MagnifyingGlassIcon
+                aria-hidden
+                className="h-4 w-4 cursor-pointer"
+                onClick={() => searchPerson(per)}
+              />
+            </div>
+          </div>
           <div>
             {/* TODO: deze elementen nog beter stylen. Onzekerheid beter weergeven, net als de `notBefore`. */}
             {per.birth?.when || per.birth?.cert}-
