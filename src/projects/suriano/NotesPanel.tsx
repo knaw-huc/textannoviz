c522fc078edced3b636b72ee2658d30da0d7c7d3
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-06-12 13:45:06 +0200

    Disable Van Gogh and Suriano notes panels with note to refactor in the future

--- src/projects/suriano/NotesPanel.tsx
+++ src/projects/suriano/NotesPanel.tsx
@@ -1,56 +1,62 @@
-import { Button } from "react-aria-components";
-import { createNoteLines } from "../../components/Text/Annotated/MarkerTooltip";
-import {
-  AnnoRepoAnnotation,
-  isNoteBody,
-  NoteBody,
-} from "../../model/AnnoRepoAnnotation";
-import { useTextStore } from "../../stores/text/text-store.ts";
+// import { Button } from "react-aria-components";
+// import { createNoteLines } from "../../components/Text/Annotated/MarkerTooltip";
+// import {
+//   AnnoRepoAnnotation,
+//   isNoteBody,
+//   NoteBody,
+// } from "../../model/AnnoRepoAnnotation";
+// import { useTextStore } from "../../stores/text/text-store.ts";
 
-type NotesPanelProps = {
-  annotations: AnnoRepoAnnotation[];
-};
+// type NotesPanelProps = {
+//   annotations: AnnoRepoAnnotation[];
+// };
 
-export function NotesPanel(props: NotesPanelProps) {
-  const textPanels = useTextStore().views;
-  const { activeFootnote, setActiveFootnote } = useTextStore();
-
-  if (!textPanels) return null;
-  const notesView = textPanels["notes"];
-
-  const footnoteAnnotations = props.annotations.filter((anno) => {
-    //Annotation type 'tei:Note' contains different types of notes: editorial notes, general notes, footnotes. Only footnotes have 'body.metadata["tei:id"]', so that's used here to filter out the footnotes.
-    return isNoteBody(anno.body) && anno.body.metadata["tei:id"];
-  });
-
-  const footnotes = footnoteAnnotations.flatMap((noteAnno) => {
-    return {
-      id: (noteAnno.body as NoteBody).metadata["tei:id"],
-      lines: createNoteLines(notesView, noteAnno.body.id),
-    };
-  });
-
-  const footnoteButtonPressHandler = (footnoteId: string) => {
-    setActiveFootnote(footnoteId);
-  };
-
+export function NotesPanel() {
   return (
-    <div className="flex flex-col items-start">
-      {footnotes.length ? (
-        footnotes.map((footnote, index) => (
-          <Button
-            className={`w-full cursor-pointer rounded-md p-2 text-left outline-none transition-all duration-300 ease-in-out hover:underline ${
-              activeFootnote === footnote.id ? "bg-brand2-300" : "bg-white"
-            }`}
-            key={index}
-            onPress={() => footnoteButtonPressHandler(footnote.id)}
-          >
-            {footnote.lines}
-          </Button>
-        ))
-      ) : (
-        <div>This letter has no footnotes.</div>
-      )}
+    <div>
+      TODO: Refactor Van Gogh notes panel to new data format introduced for
+      Israels.
     </div>
   );
+  // const textPanels = useTextStore().views;
+  // const { activeFootnote, setActiveFootnote } = useTextStore();
+
+  // if (!textPanels) return null;
+  // const notesView = textPanels["notes"];
+
+  // const footnoteAnnotations = props.annotations.filter((anno) => {
+  //   //Annotation type 'tei:Note' contains different types of notes: editorial notes, general notes, footnotes. Only footnotes have 'body.metadata["tei:id"]', so that's used here to filter out the footnotes.
+  //   return isNoteBody(anno.body) && anno.body.metadata["tei:id"];
+  // });
+
+  // const footnotes = footnoteAnnotations.flatMap((noteAnno) => {
+  //   return {
+  //     id: (noteAnno.body as NoteBody).metadata["tei:id"],
+  //     lines: createNoteLines(notesView, noteAnno.body.id),
+  //   };
+  // });
+
+  // const footnoteButtonPressHandler = (footnoteId: string) => {
+  //   setActiveFootnote(footnoteId);
+  // };
+
+  // return (
+  //   <div className="flex flex-col items-start">
+  //     {footnotes.length ? (
+  //       footnotes.map((footnote, index) => (
+  //         <Button
+  //           className={`w-full cursor-pointer rounded-md p-2 text-left outline-none transition-all duration-300 ease-in-out hover:underline ${
+  //             activeFootnote === footnote.id ? "bg-brand2-300" : "bg-white"
+  //           }`}
+  //           key={index}
+  //           onPress={() => footnoteButtonPressHandler(footnote.id)}
+  //         >
+  //           {footnote.lines}
+  //         </Button>
+  //       ))
+  //     ) : (
+  //       <div>This letter has no footnotes.</div>
+  //     )}
+  //   </div>
+  // );
 }
--- src/projects/vangogh/NotesPanel.tsx
+++ src/projects/vangogh/NotesPanel.tsx
@@ -1,15 +1,8 @@
-import { AnnotatedText } from "../../components/Text/Annotated/AnnotatedText";
-import { useTextStore } from "../../stores/text/text-store";
-
 export const NotesPanel = () => {
-  const textPanels = useTextStore().views;
-
-  const noteText = textPanels!["textNotes"];
-  console.log(noteText);
-
   return (
-    <div role="textpanel">
-      <AnnotatedText text={noteText} showDetail={false} />
+    <div>
+      TODO: Refactor Van Gogh notes panel to new data format introduced for
+      Israels.
     </div>
   );
 };
