b878ed094e752db9d40a1ac62627ba85c8da46a3
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-06-24 14:08:28 +0200

    Add Artwork entity details + moved person entity details and artwork entity details to their own component for clarity

--- src/projects/israels/annotation/EntitySummaryDetails.tsx
+++ src/projects/israels/annotation/EntitySummaryDetails.tsx
@@ -1,17 +1,63 @@
 import { EntitySummaryDetailsProps } from "../../../model/ProjectConfig";
-import { isEntity, isPersonEntity } from "./ProjectAnnotationModel";
+import {
+  Artworks,
+  isArtworkEntity,
+  isEntity,
+  isPersonEntity,
+  Persons,
+} from "./ProjectAnnotationModel";
 
 export const EntitySummaryDetails = (props: EntitySummaryDetailsProps) => {
   if (isEntity(props.body) && isPersonEntity(props.body.metadata.ref)) {
-    return (
-      <div>
-        <p className="font-bold">{props.body.metadata.ref[0].sortLabel}</p>
-        <p>
-          {props.body.metadata.ref[0].birth.when}-
-          {props.body.metadata.ref[0].death.when}
-        </p>
-      </div>
-    );
+    return <PersonEntity person={props.body.metadata.ref} />;
+  }
+
+  if (isEntity(props.body) && isArtworkEntity(props.body.metadata.ref)) {
+    return <ArtworkEntity artwork={props.body.metadata.ref} />;
   }
   return null;
+};
+
+const PersonEntity = (props: { person: Persons }) => {
+  const { person } = props;
+  return (
+    <div>
+      <p className="font-bold">{person[0].sortLabel}</p>
+      <p>
+        {person[0].birth.when}-{person[0].death.when}
+      </p>
+    </div>
+  );
+};
+
+const ArtworkEntity = (props: { artwork: Artworks }) => {
+  const { artwork } = props;
+  return (
+    <div>
+      <p className="font-bold">{artwork[0].head[1].text}</p>
+      <p>Date: {artwork[0].date.text}</p>
+      <p>Artist: {artwork[0].relation.ref.sortLabel}</p>
+      <p>
+        Size: {artwork[0].measure[0].quantity} x{" "}
+        {artwork[0].measure[1].quantity} {artwork[0].measure[0].unit}
+      </p>
+      <p>Support: {artwork[0].note[1].text}</p>
+      <p>
+        Collection:{" "}
+        {artwork[0].note
+          .filter((note) => note["tei:type"] === "collection")
+          .map((note, index) => (
+            <span key={index}>{note.text}</span>
+          ))}
+      </p>
+      <p>
+        Credits:{" "}
+        {artwork[0].note
+          .filter((note) => note["tei:type"] === "creditline")
+          .map((note, index) => (
+            <span key={index}>{note.text}</span>
+          ))}
+      </p>
+    </div>
+  );
 };
