9e7c5a026335593d19c8353aa24b80a239bf01f1
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-07-02 12:05:11 +0200

    Add environment configuration files for development and production with Israels Persons and Artworks URL

--- src/utils/viteEnvVars.ts
+++ src/utils/viteEnvVars.ts
@@ -4,6 +4,8 @@ export const getViteEnvVars = () => {
   const projectName = import.meta.env["VITE_PROJECT"] as ProjectName;
   const routerBasename = import.meta.env["VITE_ROUTER_BASENAME"] as string;
   const prodMode = import.meta.env.PROD;
+  const israelsPersonsUrl = import.meta.env.VITE_ISRAELS_PERSONS_URL;
+  const israelsArtworksUrl = import.meta.env.VITE_ISRAELS_ARTWORKS_URL;
 
   if (!projectName) {
     throw new Error("VITE_PROJECT environment variable not set in .env.");
@@ -15,5 +17,23 @@ export const getViteEnvVars = () => {
     );
   }
 
-  return { projectName, routerBasename, prodMode };
+  if (!israelsPersonsUrl) {
+    throw new Error(
+      "VITE_ISRAELS_PERSONS_URL environment variable not set in .env.development or .env.production",
+    );
+  }
+
+  if (!israelsArtworksUrl) {
+    throw new Error(
+      "VITE_ISRAELS_ARTWORKS_URL environment variable not set in .env.development or .env.production",
+    );
+  }
+
+  return {
+    projectName,
+    routerBasename,
+    prodMode,
+    israelsPersonsUrl,
+    israelsArtworksUrl,
+  };
 };
