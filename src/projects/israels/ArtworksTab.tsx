8262fd9e821b72cbd3fa13f07e4cfe255483578e
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-07-03 15:44:52 +0200

    First steps towards Artworks tab

--- src/projects/israels/ArtworksTab.tsx
+++ src/projects/israels/ArtworksTab.tsx
@@ -1,16 +1,57 @@
 import { useAnnotationStore } from "../../stores/annotation";
-import { IsraelsTeiRsBody } from "./annotation/ProjectAnnotationModel";
+import { projectConfigSelector, useProjectStore } from "../../stores/project";
+import { Any } from "../../utils/Any";
+import {
+  Artworks,
+  isArtworkEntity,
+  IsraelsTeiRsBody,
+} from "./annotation/ProjectAnnotationModel";
 
 export const ArtworksTab = () => {
   const annotations = useAnnotationStore().annotations;
+  const interfaceLang = useProjectStore(projectConfigSelector).selectedLanguage;
 
-  const artworkAnnos = annotations.filter((anno) => {
-    return (
-      anno.body.type === "tei:Rs" &&
-      (anno.body as IsraelsTeiRsBody).metadata["tei:type"] === "artwork"
-    );
-  });
-  console.log(artworkAnnos);
+  const artworkAnnos = annotations.reduce<
+    { body: IsraelsTeiRsBody & { metadata: { ref: Artworks } } }[]
+  >((acc, anno) => {
+    if (isArtworkAnno(anno)) {
+      const artworkAnno = anno as unknown as {
+        body: IsraelsTeiRsBody & { metadata: { ref: Artworks } };
+      };
+      const id = artworkAnno.body.metadata.ref[0].id;
+      if (!acc.some((a) => a.body.metadata.ref[0].id === id)) {
+        acc.push(artworkAnno);
+      }
+    }
+    return acc;
+  }, []);
 
-  return <div>Artworks</div>;
+  if (!artworkAnnos.length) return <div>No artworks in this letter.</div>;
+
+  return (
+    <>
+      {artworkAnnos.map((artwork) => (
+        <ul key={artwork.body.id}>
+          <li>
+            <div className="font-bold">
+              {artwork.body.metadata.ref[0].head[interfaceLang]}
+            </div>
+            <div>
+              <span>{artwork.body.metadata.ref[0].date.text}</span>
+            </div>
+          </li>
+        </ul>
+      ))}
+    </>
+  );
 };
+
+function isArtworkAnno(
+  anno: Any,
+): anno is { body: IsraelsTeiRsBody & { metadata: { ref: Artworks } } } {
+  return (
+    anno.body.type === "tei:Rs" &&
+    (anno.body as IsraelsTeiRsBody).metadata["tei:type"] === "artwork" &&
+    isArtworkEntity((anno.body as IsraelsTeiRsBody).metadata.ref)
+  );
+}
