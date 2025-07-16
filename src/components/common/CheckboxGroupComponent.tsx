e7cf1b7b1ecaa6d9c98e1ebb8113f6f85383b728
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-07-16 11:38:01 +0200

    Update checkbox selected colour to match other colours

--- src/components/common/CheckboxGroupComponent.tsx
+++ src/components/common/CheckboxGroupComponent.tsx
@@ -102,7 +102,7 @@ export function CheckboxComponent(props: CheckboxComponentProps) {
       <div
         aria-label="filter on "
         className={`${
-          isSelected ? "bg-brand2-600 border-brand2-600" : ""
+          isSelected ? "bg-brand2-500 border-brand2-500" : ""
         } flex h-5 w-5 flex-shrink-0 items-center justify-center rounded border-2 transition`}
       >
         {isSelected && <CheckIcon className="h-5 w-5 text-white" />}
