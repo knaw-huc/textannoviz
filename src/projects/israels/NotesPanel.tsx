937f49a87c617ad2b823bfffec48ba450ee8c908
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-08-05 12:16:49 +0200

    Simplify Israels NotesPanel now that footnoteId is no longer used

--- src/projects/israels/NotesPanel.tsx
+++ src/projects/israels/NotesPanel.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { AnnotatedText } from "../../components/Text/Annotated/AnnotatedText";
-import { NoteBody } from "../../model/AnnoRepoAnnotation";
-import { useAnnotationStore } from "../../stores/annotation";
 import {
   projectConfigSelector,
   translateProjectSelector,
@@ -14,7 +12,6 @@ export const NotesPanel = () => {
   const interfaceLang = useProjectStore(projectConfigSelector).defaultLanguage;
   const translateProject = useProjectStore(translateProjectSelector);
   const { activeFootnote } = useTextStore();
-  const annos = useAnnotationStore().annotations;
 
   React.useEffect(() => {
     if (!activeFootnote) return;
@@ -29,47 +26,23 @@ export const NotesPanel = () => {
   const notes = textNotes?.[interfaceLang];
   if (!notes) return <div>{translateProject("NO_NOTES")}</div>;
 
-  const noteAnnoBodyIds = Object.entries(notes).map(([, note]) => {
-    return note.locations.relativeTo.bodyId;
-  });
-
-  const footnoteAnnos = annos
-    .filter((anno) => {
-      return noteAnnoBodyIds.includes(anno.body.id);
-    })
-    .map((footnoteAnno) => {
-      return {
-        footnoteId: (footnoteAnno.body as NoteBody).metadata["tei:id"],
-        bodyId: footnoteAnno.body.id,
-      };
-    });
-
-  const footnotes = Object.entries(notes).flatMap(([number, note], index) => {
-    const footnoteAnno = footnoteAnnos[index];
-    return {
-      footnoteId: footnoteAnno.footnoteId,
-      footnoteNumber: number,
-      note: note,
-    };
-  });
-
   return (
     <div role="notespanel" className="flex flex-col" key={interfaceLang}>
-      {footnotes.map((footnote) => (
+      {Object.entries(notes).map(([footnoteNumber, note]) => (
         <div
-          id={footnote.footnoteNumber}
-          key={footnote.footnoteNumber}
+          id={footnoteNumber}
+          key={footnoteNumber}
           className={`flex flex-row p-2 ${
-            activeFootnote === footnote.footnoteNumber
+            activeFootnote === footnoteNumber
               ? "rounded-lg bg-[#FFCE01] transition-all duration-300"
               : "bg-white"
           }`}
         >
           <span className="mr-2 text-sm text-neutral-500">
-            {footnote.footnoteNumber}.{" "}
+            {footnoteNumber}.{" "}
           </span>
           <div className="text-sm">
-            <AnnotatedText text={footnote.note} showDetail={false} />
+            <AnnotatedText text={note} showDetail={false} />
           </div>
         </div>
       ))}
