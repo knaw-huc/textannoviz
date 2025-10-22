119012b4fdff7568c1f8da5388d7ebd637fb1306
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-10-22 11:08:53 +0200

    Change help link based on interface language for Republic

--- src/projects/republic/HelpLink.tsx
+++ src/projects/republic/HelpLink.tsx
@@ -1,10 +1,18 @@
+import { projectConfigSelector, useProjectStore } from "../../stores/project";
+
 export function HelpLink() {
+  const interfaceLang = useProjectStore(projectConfigSelector).selectedLanguage;
+
   return (
     <div className="mr-4 flex grow flex-row items-center justify-end gap-2">
       <a
         rel="noreferrer"
         target="_blank"
-        href="https://goetgevonden.nl/help/gebruik-van-de-applicatie"
+        href={
+          interfaceLang === "nl"
+            ? "https://goetgevonden.nl/help/gebruik-van-de-applicatie"
+            : "https://goetgevonden.nl/en/using-the-application/"
+        }
         className="hover:text-brand1-100 text-inherit no-underline hover:underline"
       >
         Help
