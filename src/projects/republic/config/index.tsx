e9eafb4b4a4dd5aa07e39986902a3de8f95528c5
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-10-23 11:28:52 +0200

    Disable showWebAnnoTab again for Republic

--- src/projects/republic/config/index.tsx
+++ src/projects/republic/config/index.tsx
@@ -120,7 +120,7 @@ export const republicConfig: ProjectConfig = merge({}, defaultConfig, {
     "roleName",
   ],
   showFacetFilter: true,
-  showWebAnnoTab: true,
+  showWebAnnoTab: false,
   components: {
     EntitySummary,
     MetadataPanel,
