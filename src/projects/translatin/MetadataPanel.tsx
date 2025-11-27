4491c6dd82cafbfad1dcaaf67ba380f8c38d0c68
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-11-27 14:30:40 +0100

    Rename variable in Translatin MetadataPanel

--- src/projects/translatin/MetadataPanel.tsx
+++ src/projects/translatin/MetadataPanel.tsx
@@ -8,7 +8,7 @@ type MetadataPanelProps = {
 };
 
 export const MetadataPanel = (props: MetadataPanelProps) => {
-  const manifestationAnno = props.annotations.find(
+  const documentAnno = props.annotations.find(
     // TODO: where to find metadata?
     (anno) => anno.body.type === "Document",
   );
@@ -16,7 +16,7 @@ export const MetadataPanel = (props: MetadataPanelProps) => {
   return (
     <>
       <ul className="m-0 list-none p-0">
-        {Object.entries(manifestationAnno?.body as TfLetterBody).map(
+        {Object.entries(documentAnno?.body as TfLetterBody).map(
           ([key, value], index) => (
             <li className="mb-8" key={index}>
               <div className="grid grid-cols-1">
