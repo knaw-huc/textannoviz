002672cab1bcf4287eb3785685afd8a34b183300
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2025-11-27 16:18:43 +0100

    Update Translatin search title

--- src/projects/translatin/SearchItem.tsx
+++ src/projects/translatin/SearchItem.tsx
@@ -8,19 +8,20 @@ export const SearchItem = (
   props: SearchItemProps<TranslatinSearchResultsBody>,
 ) => {
   const { createDetailUrl } = useDetailNavigation();
+
+  const searchTitle = `${props.result.author}, ${props.result.title}. ${props.result.location}, ${props.result.publisher}, ${props.result.datePublished}`;
+
   return (
     <ul className="border-brand1Grey-200 mb-4 border-b">
       <li className="mb-3 text-base">
-        <strong>{props.result.pubYear}</strong>
+        <strong>{props.result.datePublished}</strong>
       </li>
       <Link
         to={createDetailUrl(props.result._id)}
         className="hover:text-brand1-600 active:text-brand1-700 text-inherit no-underline"
       >
         <li className="divide-brand1Grey-100 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-6 w-full cursor-pointer divide-y divide-solid rounded border bg-white shadow-sm transition hover:bg-white">
-          <div className="p-4 font-semibold">
-            {`${props.result.author}, ${props.result.titleShort}. ${props.result.pubPlace}, ${props.result.publisher}, ${props.result.pubYear}`}
-          </div>
+          <div className="p-4 font-semibold">{searchTitle}</div>
           {props.result._hits?.playText.map((hit, index) => (
             <div key={index} className="hover:bg-brand1Grey-50 w-full p-4">
               <div
