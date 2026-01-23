2c3202b50bf9a50be73852c988dca59dd768fa95
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-01-23 14:21:25 +0100

    Add 'text.en' to 'origTextPanel' because some letters are written in English

--- src/projects/vangogh/TextPanels.tsx
+++ src/projects/vangogh/TextPanels.tsx
@@ -2,7 +2,9 @@ import { TextComponentTab } from "../../components/Detail/TextComponentTab";
 
 export const TextPanels = {
   origTextPanel: {
-    content: <TextComponentTab viewToRender={["text.nl", "text.fr"]} />,
+    content: (
+      <TextComponentTab viewToRender={["text.nl", "text.fr", "text.en"]} />
+    ),
   },
   transTextPanel: {
     content: <TextComponentTab viewToRender="text.en" />,
