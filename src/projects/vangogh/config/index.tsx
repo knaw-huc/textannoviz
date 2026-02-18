dfe6a410833451333417de42e826b323e85d085d
Bas Leenknegt <basleenknegt@gmail.com>
2026-02-18 09:51:02 +0100

    Fix englishVanGoghLabels import

--- src/projects/vangogh/config/index.tsx
+++ src/projects/vangogh/config/index.tsx
@@ -4,7 +4,7 @@ import {
   ProjectSpecificConfig,
 } from "../../../model/ProjectConfig";
 import projectCss from "../project.css?inline";
-import { englishVangoghLabels } from "./englishVangoghLabels";
+import { englishVangoghLabels } from "./englishVanGoghLabels";
 // import { dutchVangoghLabels } from "./dutchVangoghLabels";
 import { kunstenaarsbrievenConfig } from "../../kunstenaarsbrieven/config";
 import { Persons } from "../Persons";
