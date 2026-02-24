e640ccd6c6ab4a037452ac71ee682b1b7691bf44
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-02-24 16:21:54 +0100

    Oraties: translate some labels

--- src/projects/oraties/config/dutchOratiesLabels.ts
+++ src/projects/oraties/config/dutchOratiesLabels.ts
@@ -1,13 +1,35 @@
 import { englishLabels } from "../../default/config/englishLabels";
 
 export const dutchOratiesLabels = Object.assign({}, englishLabels, {
-  self: "Oratie",
-
   //Facet titles
   date: "Datum",
+  DATE_HELP:
+    "Filter op de datum waarop de oratie is gehouden. De weergave van de datum (dag-maand of maand-dag) wordt bepaald door de instellingen van uw computer of browser.",
   publisher: "Uitgever",
+  location: "Plaats",
+  LOCATION_HELP: "Plaats van publicatie",
+  author: "Auteur",
+  AUTHOR_HELP: "Auteur van de oratie",
 
+  //Text panel titles
+  self: "Oratie",
+  "text.orig": "Tekst",
+
+  //panels
   facs: "Facsimile",
   metadata: "Info",
+
+  //Footer
+  NAVIGATE_SEARCH_RESULTS: "Navigeer zoekresultaten",
+  SEARCH: "Zoek",
+
+  //Metadata panel titles
   notes: "Noten",
+
+  // search: "Zoek",
+  SEARCH_IN: "Zoek in",
+  SHOW_PAGE: "Toon Slide",
+  // view: "Toon",
+  VIEW_HELP:
+    "Gebruik de weergaveopties rechtsonder in het venster om elementen zoals transcriptie, metadata of facsimile weer te geven of te verbergen. Verborgen elementen worden grijs weergegeven.",
 });
