3baf1b5d9840e8a1485aefac95744a56bfb5b299
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-03-20 11:48:00 +0100

    Update local Docker dev build script

--- scripts/docker-dev.sh
+++ scripts/docker-dev.sh
@@ -2,19 +2,13 @@
 
 export PROJECT=vangogh
 
-export VERSION=dev
-export DOCKER_DOMAIN=registry.diginfra.net/tt
-export TITLE="Textannoviz - $PROJECT (DEV)"
-export TAG=${DOCKER_DOMAIN}/textannoviz-${PROJECT}-frontend:dev
+export TAG=textannoviz:dev
 #ROUTER_BASENAME needs to be set to the subdomain TAV will run at on the server. If it runs at root level, it needs to be set to "/". For the preview environment, this needs to be set to "/app".
-export ROUTER_BASENAME="/app"
+export ROUTER_BASENAME="/"
 export MODE="prod"
 
 if [[
-  -z $VERSION \
-  || -z $DOCKER_DOMAIN \
-  || -z $PROJECT \
-  || -z $TITLE \
+  -z $PROJECT \
   || -z $TAG \
   || -z $ROUTER_BASENAME \
   || -z $MODE
@@ -25,7 +19,6 @@ fi
 
 sed \
   -e "/^VITE_PROJECT=/s/=.*/=$PROJECT/" \
-  -e "/^VITE_TITLE=/s/=.*/=$TITLE/" \
   -e "/^VITE_ROUTER_BASENAME=/s|=.*|=$ROUTER_BASENAME|" \
   .env.example \
   > .env
@@ -35,7 +28,6 @@ docker build -t $TAG --platform=linux/amd64 -f deploy/Dockerfile-$MODE .
 #Reset VITE_ROUTER_BASENAME back to "/" in .env
 sed \
   -e "/^VITE_PROJECT=/s/=.*/=$PROJECT/" \
-  -e "/^VITE_TITLE=/s/=.*/=$TITLE/" \
   -e "/^VITE_ROUTER_BASENAME=/s/=.*/=\//" \
   .env.example \
   > .env
