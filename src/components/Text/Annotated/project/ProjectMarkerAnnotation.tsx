16fc9d7ecb8a72fb6a94f5ee5d774903ae306dc7
Bas Leenknegt <basleenknegt@gmail.com>
2026-04-08 14:16:30 +0200

    Use osd-iiif-viewer@0.2.1

--- src/components/Text/Annotated/project/ProjectMarkerAnnotation.tsx
+++ src/components/Text/Annotated/project/ProjectMarkerAnnotation.tsx
@@ -214,7 +214,7 @@ export function hasMarkerPositions(annotation: BroccoliRelativeAnno) {
   return annotation.end === annotation.begin;
 }
 
-function zoomToRegion(viewer: OpenSeadragon.Viewer, region: string) {
+function zoomToRegion(viewer: ReturnType<typeof useViewer>, region: string) {
   const [x, y, w, h] = region.split(",").map((s) => Number(s));
   const rect = viewer.viewport.imageToViewportRectangle(x, y, w, h);
   viewer.viewport.fitBounds(rect);
