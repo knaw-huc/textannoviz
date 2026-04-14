5419a953d55493884911bcdc1f6e1c21b03223a1
Bas Leenknegt <basleenknegt@gmail.com>
2026-04-14 09:24:04 +0200

    Fix zoomToRegion signature

--- src/components/Text/Annotated/MarkerAnnotation.tsx
+++ src/components/Text/Annotated/MarkerAnnotation.tsx
@@ -160,7 +160,10 @@ export function InsertMarkerAnnotation(props: { marker: MarkerSegment }) {
   return <projectConfig.components.InsertMarkerAnnotation marker={marker} />;
 }
 
-function zoomToRegion(viewer: ReturnType<typeof useViewer>, region: string) {
+function zoomToRegion(
+  viewer: NonNullable<ReturnType<typeof useViewer>>,
+  region: string,
+) {
   const [x, y, w, h] = region.split(",").map((s) => Number(s));
   const rect = viewer.viewport.imageToViewportRectangle(x, y, w, h);
   viewer.viewport.fitBounds(rect);
