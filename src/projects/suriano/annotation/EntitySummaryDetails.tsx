81bfb865c19a3277ee06ce6fd740a232b7e98681
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-04-15 15:35:19 +0200

    suriano: extract headerClass to const

--- src/projects/suriano/annotation/EntitySummaryDetails.tsx
+++ src/projects/suriano/annotation/EntitySummaryDetails.tsx
@@ -14,42 +14,42 @@ export const EntitySummaryDetails = (props: EntitySummaryDetailsProps) => {
 const PersonEntity = (props: { persons: Person[] }) => {
   //FIXME: this adds all persons together with only 1 search button. This happens because it's 1 annotation with multiple persons in the body(.metadata).ref. In other projects, every entity had it's own annotation.
   const { persons } = props;
-  console.log(persons);
+  const headerClass = "italic text-gray-500";
   return (
     <>
       {persons.map((pers) => (
         <div key={pers.id} className="flex flex-col gap-4">
           <div>
-            <p className="italic text-gray-500">Primary Name</p>
+            <p className={headerClass}>Primary Name</p>
             <p>{pers.sortLabel}</p>
           </div>
           {pers.note?.en.shortdesc && (
             <div>
-              <p className="italic text-gray-500">Occupations, roles, titles</p>
+              <p className={headerClass}>Occupations, roles, titles</p>
               <p>{pers.note.en.shortdesc}</p>
             </div>
           )}
           {pers.birth && (
             <div>
-              <p className="italic text-gray-500">Date of birth</p>
+              <p className={headerClass}>Date of birth</p>
               <p>{pers.birth.when}</p>
             </div>
           )}
           {pers.death && (
             <div>
-              <p className="italic text-gray-500">Date of death</p>
+              <p className={headerClass}>Date of death</p>
               <p>{pers.death.when}</p>
             </div>
           )}
           {pers.note?.en.biographic && (
             <div>
-              <p className="italic text-gray-500">General notes</p>
+              <p className={headerClass}>General notes</p>
               <p>{pers.note.en.biographic}</p>
             </div>
           )}
           {pers.note?.en.bibliography && (
             <div>
-              <p className="italic text-gray-500">
+              <p className={headerClass}>
                 Bibliography: secondary literature and websites
               </p>
               {typeof pers.note.en.bibliography === "string" && (
