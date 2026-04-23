cd0832866c4903b1790fcdd7791de3467a60ec4b
Bas Leenknegt <basleenknegt@gmail.com>
2026-04-23 14:58:04 +0200

    Rename to .annotated-text

--- src/components/Text/Annotated/ProjectAnnotatedText.tsx
+++ src/components/Text/Annotated/ProjectAnnotatedText.tsx
@@ -83,7 +83,7 @@ export const ProjectAnnotatedText = (props: TextHighlightingProps) => {
     });
   offsets.push(...blockAnnotations);
   return (
-    <div className="project-annotated-text whitespace-pre-wrap">
+    <div className="annotated-text whitespace-pre-wrap">
       <AnnotatedText
         components={projectConfig.annotatedTextComponents}
         text={textBody}
