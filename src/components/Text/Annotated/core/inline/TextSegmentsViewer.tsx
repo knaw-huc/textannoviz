65f2890fdc3357d5d1fe5291af03e7be8e1b5ddc
Bas Leenknegt <basleenknegt@gmail.com>
2026-04-23 13:04:47 +0200

    Remove text-segment selector

--- src/components/Text/Annotated/core/inline/TextSegmentsViewer.tsx
+++ src/components/Text/Annotated/core/inline/TextSegmentsViewer.tsx
@@ -3,20 +3,14 @@ import { Segment } from "../AnnotationModel.ts";
 
 export type TextSegmentsViewerProps = {
   segments: Segment[];
-  className?: string;
 };
 
 export function TextSegmentsViewer(props: TextSegmentsViewerProps) {
-  const classes = ["text-segment"];
-  if (props.className) {
-    classes.push(props.className);
-  }
-
   return (
-    <span className={classes.join(" ")}>
+    <>
       {props.segments.map((segment, i) => (
         <TextSegment key={i} segment={segment} />
       ))}
-    </span>
+    </>
   );
 }
