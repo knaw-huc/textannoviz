b5131a7cc042c737cd3abda7e2446ea30fc452a3
Bas Leenknegt <basleenknegt@gmail.com>
2026-04-23 19:54:35 +0200

    Fix whitespace between lines in a paragraph: only hide between blocks

--- src/projects/kunstenaarsbrieven/annotation/KunstenaarsbrievenHighlight.tsx
+++ src/projects/kunstenaarsbrieven/annotation/KunstenaarsbrievenHighlight.tsx
@@ -1,20 +1,9 @@
 import { HighlightBody } from "../../../components/Text/Annotated/utils/highlightBodyGuards.ts";
 import { HighlightProps } from "../../../components/Text/Annotated/core";
-import { isWhitespace } from "./ProjectAnnotationModel.ts";
 import { DefaultHighlight } from "../../default/annotation/highlight/DefaultHighlight.tsx";
 
 export function KunstenaarsbrievenHighlight(
   props: HighlightProps<HighlightBody>,
 ) {
-  const { highlights, children } = props;
-
-  const isWhitespaceMarkup = highlights.every(
-    (h) => isWhitespace(h.body) && h.body.isTextSuffix,
-  );
-
-  if (isWhitespaceMarkup) {
-    return <span className="whitespace-markup">{children}</span>;
-  }
-
   return <DefaultHighlight {...props} />;
 }
