b8748623e8c6848a94c13883dcca971382d548ac
doppenb <basdoppen@gmail.com>
2026-05-12 10:09:48 +0200

    replace ul + reorder li - link

--- src/projects/suriano/SearchItem.tsx
+++ src/projects/suriano/SearchItem.tsx
@@ -22,31 +22,29 @@ export const SearchItem = (
   const { createDetailUrl } = useDetailNavigation();
 
   return (
-    <ul className="border-brand1Grey-200 mb-4 border-b">
+    <li className="divide-brand1Grey-100 border-brand1Grey-200 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-4 mb-6 w-full cursor-pointer divide-y divide-solid rounded border border-b bg-white shadow-sm transition hover:bg-white">
       <Link
         to={createDetailUrl(props.result._id)}
         className="hover:text-brand1-600 active:text-brand1-700 text-inherit no-underline"
       >
-        <li className="divide-brand1Grey-100 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-6 w-full cursor-pointer divide-y divide-solid rounded border bg-white shadow-sm transition hover:bg-white">
-          <div className="flex flex-col gap-1 p-4">
-            <p className="font-semibold">{searchItemTitle}</p>
-            {props.result.summary.length > 0 ? (
-              <Summary
-                summary={props.result.summary}
-                summaryHits={props.result._hits?.summary}
-              />
-            ) : null}
+        <div className="flex flex-col gap-1 p-4">
+          <p className="font-semibold">{searchItemTitle}</p>
+          {props.result.summary.length > 0 ? (
+            <Summary
+              summary={props.result.summary}
+              summaryHits={props.result._hits?.summary}
+            />
+          ) : null}
+        </div>
+        {props.result._hits?.text?.map((hit, index) => (
+          <div key={index} className="hover:bg-brand1Grey-50 w-full p-4">
+            <div
+              className="mb-1 font-serif text-base"
+              dangerouslySetInnerHTML={{ __html: hit }}
+            />
           </div>
-          {props.result._hits?.text?.map((hit, index) => (
-            <div key={index} className="hover:bg-brand1Grey-50 w-full p-4">
-              <div
-                className="mb-1 font-serif text-base"
-                dangerouslySetInnerHTML={{ __html: hit }}
-              />
-            </div>
-          ))}
-        </li>
+        ))}
       </Link>
-    </ul>
+    </li>
   );
 };
