afa36be610d9adfe0d84c0831671de080dc11b38
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-05-12 10:55:55 +0200

    Add additional data to Van Gogh artwork card

--- src/projects/vangogh/artworks/ArtworkCard.tsx
+++ src/projects/vangogh/artworks/ArtworkCard.tsx
@@ -1,4 +1,5 @@
 import { LanguageCode } from "../../../model/Language";
+import { useTranslateProject } from "../../../stores/project";
 import { Artwork } from "../../kunstenaarsbrieven/annotation/ProjectAnnotationModel";
 
 export function ArtworkCard(props: {
@@ -6,6 +7,7 @@ export function ArtworkCard(props: {
   interfaceLang: LanguageCode;
 }) {
   const { artwork, interfaceLang } = props;
+  const translateProject = useTranslateProject();
 
   return (
     <div className="h-auto rounded bg-neutral-50 p-5 shadow-sm">
@@ -14,12 +16,88 @@ export function ArtworkCard(props: {
           ? artwork.head[interfaceLang]
           : `${artwork.id} has no/empty/incorrect 'head' element in XML!`}
       </div>
+      {artwork.relation
+        ? artwork.relation.map((relation, key) => (
+            <div key={key}>
+              {translateProject(relation.name)}: {relation.label}
+            </div>
+          ))
+        : null}
+      {artwork.date?.text ? (
+        <div>
+          {translateProject("date")}: {artwork.date.text}
+        </div>
+      ) : null}
+      {artwork.measure ? (
+        <div>
+          {translateProject("size")}: {artwork.measure[0].quantity} x{" "}
+          {artwork.measure[1].quantity} {artwork.measure[0].unit}
+        </div>
+      ) : null}
+      {artwork.note?.some((note) => note.type === "technical") ? (
+        <div>
+          {translateProject("support")}:{" "}
+          {Object.values(artwork.note)
+            .filter((value) => value.type === "technical")
+            .map((value, index) => (
+              <span key={index}>{value.text}</span>
+            ))}
+        </div>
+      ) : null}
+      {artwork.note?.some((note) => note.type === "collection") ? (
+        <div>
+          {translateProject("collection")}:{" "}
+          {Object.values(artwork.note)
+            .filter((value) => value.type === "collection")
+            .map((value, index) => (
+              <span key={index}>{value.text}</span>
+            ))}
+        </div>
+      ) : null}
+      {artwork.idno?.some((idno) => idno.type === "inventory") ? (
+        <div>
+          {translateProject("inventory")}:{" "}
+          {Object.values(artwork.idno)
+            .filter((value) => value.type === "inventory")
+            .map((value, index) => (
+              <span key={index}>{value.text}</span>
+            ))}
+        </div>
+      ) : null}
+      {artwork.note?.some((note) => note.type === "creditline") ? (
+        <div>
+          {translateProject("credits")}:{" "}
+          {Object.values(artwork.note)
+            .filter((value) => value.type === "creditline")
+            .map((value, index) => (
+              <span key={index}>{value.text}</span>
+            ))}
+        </div>
+      ) : null}
+      {artwork.bibl ? (
+        <div>
+          In: <span className="italic">{artwork.bibl.title}</span>
+          <span>
+            {" "}
+            {artwork.bibl.biblScope
+              ?.filter((scope) => scope.unit === "volume")
+              .map((scope) => scope.text)}
+          </span>
+          <span>
+            {" "}
+            ({artwork.bibl.date}),{" "}
+            {artwork.bibl.biblScope
+              ?.filter((scope) => scope.unit === "page")
+              .map((scope) => scope.text)}
+          </span>
+        </div>
+      ) : null}
       {artwork.graphic && (
         <img
           src={`${artwork.graphic.url}/full/200,/0/default.jpg`}
           alt={artwork.head[interfaceLang]}
           loading="lazy"
-          className="rounded"
+          className="mt-4 rounded"
         />
       )}
     </div>
