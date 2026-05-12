f213aec60000d60191fb5204f1bbc34f943ea607
doppenb <basdoppen@gmail.com>
2026-05-12 10:09:48 +0200

    replace ul + reorder li - link

--- src/projects/kunstenaarsbrieven/SearchItem.tsx
+++ src/projects/kunstenaarsbrieven/SearchItem.tsx
@@ -22,42 +22,40 @@ export const SearchItem = (props: KunstenaarsbrievenSearchItemProps) => {
 
   const queryUrlParam = encodeObject(_.pick(props.query, "fullText"));
   return (
-    <ul className="border-brand1Grey-200 mb-4 border-b">
-      <li className="divide-brand1Grey-100 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-6 w-full divide-y divide-solid rounded border bg-white shadow-sm transition hover:bg-white">
-        <Link
-          to={`/detail/${props.result._id}?${QUERY}=${queryUrlParam}`}
-          className="hover:text-brand1-600 active:text-brand1-700 cursor-pointer text-inherit no-underline"
-        >
-          <div className="flex flex-col p-4">
-            <div className="font-semibold">{props.searchItemTitle}</div>
-            {props.result.type === "letter" ? (
-              <div className="text-brand1Grey-600 italic">
-                {translateProject("LET_NUM")}: {letterNum}
-              </div>
-            ) : null}
-          </div>
-        </Link>
-        {props.result._hits
-          ? Object.entries(props.result._hits).map(([viewType, hits]) => {
-              return (
-                <div key={viewType} className="w-full p-4">
-                  <div className="mb-1 font-semibold">
-                    {translateProject(viewType)}:
-                  </div>
-                  <ul className="ml-4 list-disc">
-                    {hits.map((hit, index) => (
-                      <li
-                        className="mb-1 ml-4 list-disc font-serif text-base"
-                        dangerouslySetInnerHTML={{ __html: hit }}
-                        key={index}
-                      />
-                    ))}
-                  </ul>
+    <li className="divide-brand1Grey-100 border-brand1Grey-200 border-brand1Grey-50 hover:divide-brand1Grey-200 hover:border-brand1Grey-200 mb-4 mb-6 w-full divide-y divide-solid rounded border border-b bg-white shadow-sm transition hover:bg-white">
+      <Link
+        to={`/detail/${props.result._id}?${QUERY}=${queryUrlParam}`}
+        className="hover:text-brand1-600 active:text-brand1-700 cursor-pointer text-inherit no-underline"
+      >
+        <div className="flex flex-col p-4">
+          <div className="font-semibold">{props.searchItemTitle}</div>
+          {props.result.type === "letter" ? (
+            <div className="text-brand1Grey-600 italic">
+              {translateProject("LET_NUM")}: {letterNum}
+            </div>
+          ) : null}
+        </div>
+      </Link>
+      {props.result._hits
+        ? Object.entries(props.result._hits).map(([viewType, hits]) => {
+            return (
+              <div key={viewType} className="w-full p-4">
+                <div className="mb-1 font-semibold">
+                  {translateProject(viewType)}:
                 </div>
-              );
-            })
-          : null}
-      </li>
-    </ul>
+                <ul className="ml-4 list-disc">
+                  {hits.map((hit, index) => (
+                    <li
+                      className="mb-1 ml-4 list-disc font-serif text-base"
+                      dangerouslySetInnerHTML={{ __html: hit }}
+                      key={index}
+                    />
+                  ))}
+                </ul>
+              </div>
+            );
+          })
+        : null}
+    </li>
   );
 };
