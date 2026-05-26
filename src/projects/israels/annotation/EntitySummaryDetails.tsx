8db8048caa838d7f9feae12bcd816083e06df9a0
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-05-26 15:19:04 +0200

    Fix Israels EntitySummaryDetails

--- src/projects/israels/annotation/EntitySummaryDetails.tsx
+++ src/projects/israels/annotation/EntitySummaryDetails.tsx
@@ -1,4 +1,3 @@
-import { EntitySummaryDetailsProps } from "../../../model/ProjectConfig";
 import {
   projectConfigSelector,
   useProjectStore,
@@ -6,24 +5,25 @@ import {
 } from "../../../stores/project";
 import {
   Artwork,
-  isArtwork,
-  isPerson,
   Person,
+  PersonTeiRef,
 } from "../../kunstenaarsbrieven/annotation/ProjectAnnotationModel.ts";
 
-export const EntitySummaryDetails = (props: EntitySummaryDetailsProps) => {
-  if (isPerson(props.body)) {
-    return <PersonEntity persons={[props.body["tei:ref"]]} />;
+export const EntitySummaryDetails = (props: {
+  entityBody: PersonTeiRef | Artwork;
+  entityCategory: string;
+}) => {
+  if (props.entityCategory === "PER") {
+    return <PersonEntity persons={[props.entityBody as Person]} />;
   }
 
-  if (isArtwork(props.body)) {
-    return <ArtworkEntity artworks={[props.body["tei:ref"]]} />;
+  if (props.entityCategory === "ART") {
+    return <ArtworkEntity artworks={[props.entityBody as Artwork]} />;
   }
   return null;
 };
 
 const PersonEntity = (props: { persons: Person[] }) => {
-  //FIXME: this adds all persons together with only 1 search button. This happens because it's 1 annotation with multiple persons in the body(.metadata).ref. In other projects, every entity had it's own annotation.
   const { persons } = props;
   return (
     <>
