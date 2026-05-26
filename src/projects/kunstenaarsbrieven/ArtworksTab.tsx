c230af14c6fbc29183e77bd0ed3fbc6a99dd9101
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-05-26 15:42:42 +0200

    Disable artwork filtering in ArtworksTab

--- src/projects/kunstenaarsbrieven/ArtworksTab.tsx
+++ src/projects/kunstenaarsbrieven/ArtworksTab.tsx
@@ -30,11 +30,12 @@ export const ArtworksTab = () => {
   if (!artworkAnnos.length) return <div>{translateProject("NO_ARTWORKS")}</div>;
 
   // 12022026: SvD: Temp fix to filter out arrays in arrays. When there are 2 references in 1 ref element, it becomes an array in an array. It should be two separate entries.
-  const filteredArtw = artworkAnnos.filter((artw) => !Array.isArray(artw));
+  // 26052026: SvD: disabled for now to see if this is still an issue
+  // const filteredArtw = artworkAnnos.filter((artw) => !Array.isArray(artw));
 
   return (
     <>
-      {filteredArtw.map((artwork) => (
+      {artworkAnnos.map((artwork) => (
         <ul key={artwork.id}>
           <li>
             {artwork.graphic ? (
