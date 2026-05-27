bf7e34152e24bc7d381d1fb634d7a917e2878b21
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-05-27 15:28:09 +0200

    Normalise return type when response of fetch is not 'ok'

--- src/projects/vangogh/artworks/fetchArtworks.ts
+++ src/projects/vangogh/artworks/fetchArtworks.ts
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import { ProjectConfig } from "../../../model/ProjectConfig";
 import { Artwork } from "../../kunstenaarsbrieven/annotation/ProjectAnnotationModel";
 import { ArtworkData } from "./Artworks";
@@ -10,16 +11,22 @@ export async function fetchArtworks(
     const results = await Promise.all(
       sources.map(async (source) => {
         const response = await fetch(source.url, { signal });
-        if (!response.ok) throw new Error(`Failed to fetch ${source.key}`);
+        if (!response.ok) {
+          const error = await response.json();
+          toast(`${error.message}`, { type: "error" });
+          return null;
+        }
         const json: Artwork[] = await response.json();
         return { key: source.key, json };
       }),
     );
 
-    const finalObject = results.reduce((acc, current) => {
-      acc[current.key] = current.json;
-      return acc;
-    }, {} as ArtworkData);
+    const finalObject = results
+      .filter((result) => result !== null)
+      .reduce((acc, current) => {
+        acc[current.key] = current.json;
+        return acc;
+      }, {} as ArtworkData);
 
     return finalObject;
   } catch (error) {
