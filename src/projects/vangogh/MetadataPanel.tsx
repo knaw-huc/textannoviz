55aa891be0fedc08f46ca080e147d1c817bd5c65
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-06-11 14:48:49 +0200

    Add transcrSource, dating, and remarks notes to Van Gogh MetadataPanel

--- src/projects/vangogh/MetadataPanel.tsx
+++ src/projects/vangogh/MetadataPanel.tsx
@@ -1,13 +1,16 @@
+import { ProjectAnnotatedText } from "../../components/Text/Annotated/ProjectAnnotatedText.tsx";
 import { AnnoRepoAnnotation } from "../../model/AnnoRepoAnnotation";
 import { useTranslateProject } from "../../stores/project";
 import { gridOneColumn } from "../../utils/gridOneColumn";
 import { findLetterBody } from "../kunstenaarsbrieven/annotation/ProjectAnnotationModel.ts";
+import { useKunstenaarsbrievenTextViews } from "../kunstenaarsbrieven/text/useKunstenaarsbrievenTextViews.ts";
 
 type RenderMetadataPanelProps = {
   annotations: AnnoRepoAnnotation[];
 };
 
 export const MetadataPanel = (props: RenderMetadataPanelProps) => {
+  const textViews = useKunstenaarsbrievenTextViews();
   const translateProject = useTranslateProject();
 
   const letterAnnoBody = findLetterBody(props.annotations);
@@ -15,6 +18,10 @@ export const MetadataPanel = (props: RenderMetadataPanelProps) => {
   const { n, identifier, recipient, sender } = letterAnnoBody ?? {};
 
   const labelStyling = "text-neutral-500 uppercase text-sm";
+
+  const transcrSourceText = textViews?.transcrSource?.en;
+  const datingText = textViews?.dating?.en;
+  const remarksText = textViews?.remarks?.en;
 
   return (
     <>
@@ -53,6 +60,39 @@ export const MetadataPanel = (props: RenderMetadataPanelProps) => {
                 {Array.isArray(recipient) ? recipient.join(", ") : recipient}
               </div>
             </li>
+            {datingText?.body.length ? (
+              <li className="mb-8">
+                <div className={gridOneColumn}>
+                  <div className={labelStyling}>
+                    {translateProject("dating")}:{" "}
+                  </div>
+                  <ProjectAnnotatedText text={datingText} showDetail={false} />
+                </div>
+              </li>
+            ) : null}
+            {transcrSourceText?.body.length ? (
+              <li className="mb-8">
+                <div className={gridOneColumn}>
+                  <div className={labelStyling}>
+                    {translateProject("transcrSource")}:{" "}
+                  </div>
+                  <ProjectAnnotatedText
+                    text={transcrSourceText}
+                    showDetail={false}
+                  />
+                </div>
+              </li>
+            ) : null}
+            {remarksText?.body.length ? (
+              <li className="mb-8">
+                <div className={gridOneColumn}>
+                  <div className={labelStyling}>
+                    {translateProject("remarks")}:{" "}
+                  </div>
+                  <ProjectAnnotatedText text={remarksText} showDetail={false} />
+                </div>
+              </li>
+            ) : null}
           </>
         ) : (
           translateProject("NO_DATA")
