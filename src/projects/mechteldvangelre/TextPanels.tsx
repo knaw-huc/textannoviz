344bdc11af2a41a78f303e674317bfb0b7d9ec13
Sebastiaan van Daalen <sebastiaan.van.daalen@di.huc.knaw.nl>
2026-08-12 16:50:51 +0200

    fix: update origTextPanel viewToRender Mechteld van Gelre to Dutch only

--- src/projects/mechteldvangelre/TextPanels.tsx
+++ src/projects/mechteldvangelre/TextPanels.tsx
@@ -2,9 +2,7 @@ import { TextComponentTab } from "../../components/Detail/TextComponentTab";
 
 export const TextPanels = {
   origTextPanel: {
-    content: (
-      <TextComponentTab viewToRender={["text.nl", "text.fr", "text.en"]} />
-    ),
+    content: <TextComponentTab viewToRender={"text.nl"} />,
   },
   transTextPanel: {
     content: <TextComponentTab viewToRender="text.en" />,
